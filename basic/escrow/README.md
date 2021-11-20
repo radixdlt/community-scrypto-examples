472211976d1c7d76ac0e8b485d87de299a718d08
Clement Bisaillon <cbisaillon@petalmd.com>
2021-11-20 14:57:53 -0500

    Cleaned code

--- basic/escrow/README.md
+++ basic/escrow/README.md
@@ -6,7 +6,7 @@
 1. `resim new-account` -> save address in $acc1 and public key in $pub1
 1. `resim new-account` -> save address in $acc2 and public key in $pub2
 1. `resim set-default-account $acc2 $pub2`
-1. Create a new token to test with: `resim new-resource-fixed 8000000000 --description "The first Doge project on Radix" --name "DogeCube" --symbol "DGC"` -> save address in $dgc
+1. Create a new token to test with: `resim new-token-fixed 8000000000 --description "The first Doge project on Radix" --name "DogeCube" --symbol "DGC"` -> save address in $dgc
 1. `resim set-default-account $acc1 $pub1` 
 1. `scrypto build`
 1. `resim publish .` -> save package address in $package
@@ -14,14 +14,14 @@
 ## Trading DGC for XRD through the escrow
 $acc1 will be account A and $acc2 will be account B. A wants to trade 500 XRD for 10 DGC
 
-1. Instantiate the component for the trade: `resim call-function $package Escrow new $xrd $dgc $acc1 $acc2` -> save the address in $component
-1. Send the 500 XRD from account A: `resim call-method $component put_tokens 500,$xrd 1,[badge_of_account_a]`
+1. Instantiate the component for the trade: `resim call-function $package Escrow new $xrd $dgc $acc1 $acc2` -> save the component's address in $component, the first resourceDef into $badgeA and the second into $badgeB
+1. Send the 500 XRD from account A: `resim call-method $component put_tokens 500,$xrd 1,$badgeA`
 1. `resim set-default-account $acc2 $pub2`
-1. Send the 10 DGC from account B: `resim call-method $component put_tokens 10,$dgc 1,[badge_of_account_b]`
-1. Accept trade from account B: `resim call-method $component accept 1,[badge_of_account]`
+1. Send the 10 DGC from account B: `resim call-method $component put_tokens 10,$dgc 1,$badgeB`
+1. Accept trade from account B: `resim call-method $component accept 1,$badgeB`
 1. `resim set-default-account $acc1 $pub1`
-1. Accept Trade from account A: `resim call-method $component accept 1,[badge_of_account]`
-1. Look at the balances of both accounts, the trade should have worked !
+1. Accept Trade from account A: `resim call-method $component accept 1,$badgeA`
+1. Look at the balances of both accounts, the trade should have worked ! (`resim show $acc1` and `resim show $acc2`)
 
 ## To cancel the trade (can only be done before the two parties accepted)
 1. `resim call-method $component cancel 1,[badge_of_account]`
--- basic/escrow/src/lib.rs
+++ basic/escrow/src/lib.rs
@@ -4,10 +4,10 @@ blueprint! {
     struct Escrow {
         token_a: Vault,
         token_b: Vault,
-        token_a_account: Account,
-        token_b_account: Account,
-        account_a_badge_ressource: ResourceDef,
-        account_b_badge_ressource: ResourceDef,
+        token_a_account: Address,
+        token_b_account: Address,
+        account_a_badge: ResourceDef,
+        account_b_badge: ResourceDef,
         account_a_accepted: bool,
         account_b_accepted: bool,
         trade_canceled: bool
@@ -15,6 +15,7 @@ blueprint! {
 
     impl Escrow {
         pub fn new(token_a_address: Address, token_b_address: Address, token_a_account_address: Address, token_b_account_address: Address) -> Component {
+            // Create the badges that will allow the component to authenticate the users
             let account_a_badge = ResourceBuilder::new()
                 .metadata("symbol", "BADGE A")
                 .new_badge_fixed(1);
@@ -22,23 +23,21 @@ blueprint! {
             let account_b_badge = ResourceBuilder::new()
                 .metadata("symbol", "BADGE B")
                 .new_badge_fixed(1);
-            
-            let a_resource_def = account_a_badge.resource_def();
-            let b_resource_def = account_b_badge.resource_def();
 
-            let token_a_account = Account::from(token_a_account_address);
-            let token_b_account = Account::from(token_b_account_address);
+            let account_a_badge_resource = account_a_badge.resource_def();
+            let account_b_badge_resource = account_b_badge.resource_def();
 
-            token_a_account.deposit(account_a_badge);
-            token_b_account.deposit(account_b_badge);
+            // Send the authentication badges to both users
+            Account::from(token_a_account_address).deposit(account_a_badge);
+            Account::from(token_b_account_address).deposit(account_b_badge);
 
             Self {
                 token_a: Vault::new(ResourceDef::from(token_a_address)),
                 token_b: Vault::new(ResourceDef::from(token_b_address)),
-                token_a_account: token_a_account,
-                token_b_account: token_b_account,
-                account_a_badge_ressource: a_resource_def,
-                account_b_badge_ressource: b_resource_def,
+                token_a_account: token_a_account_address,
+                token_b_account: token_b_account_address,
+                account_a_badge: account_a_badge_resource,
+                account_b_badge: account_b_badge_resource,
                 account_a_accepted: false,
                 account_b_accepted: false,
                 trade_canceled: false
@@ -46,91 +45,79 @@ blueprint! {
             .instantiate()
         }
 
-        fn authorize(&self, badge: &BucketRef) -> bool {
-            return badge.resource_def() == self.account_a_badge_ressource 
-                || badge.resource_def() == self.account_b_badge_ressource
+        // Make sure the user_id matches either account_a or account_b badge
+        fn authorize(&self, user_id: Address) -> bool {
+            return user_id == self.account_a_badge.address()
+                || user_id == self.account_b_badge.address()
         }
 
-        fn can_add_tokens(&self, badge_resource: ResourceDef) -> bool {
-            (self.token_a.is_empty() && badge_resource == self.account_a_badge_ressource) 
-            || (self.token_b.is_empty() && badge_resource == self.account_b_badge_ressource)
+        // Check if the user can add their tokens
+        fn can_add_tokens(&self, user_id: Address) -> bool {
+            (self.token_a.is_empty() && user_id == self.account_a_badge.address()) 
+            || (self.token_b.is_empty() && user_id == self.account_b_badge.address())
         }
 
-        fn all_parties_accepted(&self) -> bool {
-            self.account_a_accepted && self.account_b_accepted
-        }
+        // Allow the users to put the tokens they want to trade inside the component's vault
+        pub fn put_tokens(&mut self, tokens: Bucket, badge: BucketRef) {
+            let user_id = Self::get_user_id(badge);
 
-        fn send_tokens(&self) {
-            if self.all_parties_accepted() {
-                self.token_a_account.deposit(self.token_b.take_all());
-                self.token_b_account.deposit(self.token_a.take_all());
-            }
-        }
+            scrypto_assert!(self.authorize(user_id), "You are not authorized !");
+            scrypto_assert!(self.can_add_tokens(user_id), "You have already added you tokens to the escrow");
+            scrypto_assert!(!self.trade_canceled, "The trade was canceled");
 
-        pub fn put_tokens(&self, tokens: Bucket, badge: BucketRef) {
-            scrypto_assert!(self.authorize(&badge), "You are not authorized !");
-
-            scrypto_assert!(
-                !self.trade_canceled,
-                "The trade was canceled"
-            );
-
-            scrypto_assert!(
-                self.can_add_tokens(badge.resource_def()),
-                "Both tokens have already been added to the escrow"
-            );
-
-            if badge.resource_def() == self.account_a_badge_ressource {
+            if user_id == self.account_a_badge.address() {
                 self.token_a.put(tokens);
-            } else if badge.resource_def() == self.account_b_badge_ressource {
+            } else if user_id == self.account_b_badge.address() {
                 self.token_b.put(tokens);
+            } else {
+                scrypto_abort("Not authorized !");
             }
-
-            badge.drop();
         }
 
+        // Allow the users to accept the trade, after both parties added their tokens to the vault
         pub fn accept(&mut self, badge: BucketRef) {
-            scrypto_assert!(self.authorize(&badge), "You are not authorized !");
+            let user_id = Self::get_user_id(badge);
 
-            scrypto_assert!(
-                !self.trade_canceled,
-                "The trade was canceled"
-            );
-            
-            scrypto_assert!(
-                !self.can_add_tokens(badge.resource_def()),
-                "Both tokens have already been added to the escrow"
-            );
+            scrypto_assert!(self.authorize(user_id), "You are not authorized !");
+            scrypto_assert!(!self.can_add_tokens(user_id), "Both parties must add their tokens before you can accept");
+            scrypto_assert!(!self.trade_canceled, "The trade was canceled");
 
-            if badge.resource_def() == self.account_a_badge_ressource {
+            if user_id == self.account_a_badge.address() {
                 scrypto_assert!(!self.account_a_accepted, "You already accepted the offer !");
                 self.account_a_accepted = true;
-            } else if badge.resource_def() == self.account_b_badge_ressource {
+            } else if user_id == self.account_b_badge.address() {
                 scrypto_assert!(!self.account_b_accepted, "You already accepted the offer !");
                 self.account_b_accepted = true;
+            } else {
+                scrypto_abort("Not authorized !");
             }
 
-            self.send_tokens();
-            badge.drop();
+            // Send the tokens if both parties accepted
+            if self.account_a_accepted && self.account_b_accepted {
+                Account::from(self.token_a_account).deposit(self.token_b.take_all());
+                Account::from(self.token_b_account).deposit(self.token_a.take_all());
+            }
         }
 
-        #[auth(account_a_badge_ressource, account_b_badge_ressource)]
+        // Cancel the trade, must be done before both parties accepted
+        #[auth(account_a_badge, account_b_badge)]
         pub fn cancel(&mut self) {
-            scrypto_assert!(
-                !(self.account_a_accepted && self.account_b_accepted),
-                "The trade is already over, everyone accepted"
-            );
-
-            scrypto_assert!(
-                !self.trade_canceled,
-                "The trade is already canceled"
-            );
+            scrypto_assert!(!(self.account_a_accepted && self.account_b_accepted), "The trade is already over, everyone accepted");
+            scrypto_assert!(!self.trade_canceled, "The trade is already canceled");
 
             self.trade_canceled = true;
 
-            // Give back the stored tokens
-            self.token_a_account.deposit(self.token_a.take_all());
-            self.token_b_account.deposit(self.token_b.take_all());
+            // Give back the stored tokens to their original owner
+            Account::from(self.token_a_account).deposit(self.token_a.take_all());
+            Account::from(self.token_b_account).deposit(self.token_b.take_all());
+        }
+
+        // Get user id from the provided badge
+        fn get_user_id(badge: BucketRef) -> Address {
+            scrypto_assert!(badge.amount() > 0.into(), "Invalid user proof");
+            let user_id = badge.resource_address();
+            badge.drop();
+            user_id
         }
     }
 }
