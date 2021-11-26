0ae4e2a33ca4a331289d57e903877995a35a98a9
Charlie Cooper <charliecooper98@gmail.com>
2021-11-26 21:44:30 +0000

    Completed logic and added readme file

--- basic/digital-library/README.md
+++ basic/digital-library/README.md
@@ -2,28 +2,113 @@
 
 This example implements a simple digital library of books.
 
-## Instructions
+There are two user types:
 
-`resim publish .`
+```rust
+let librarian_badge_bucket = ResourceBuilder::new()
+    .metadata("name", "Librarian Badge")
+    .metadata("symbol", "LB")
+    .new_badge_fixed(1);
+```
 
-`export package=`
+- single librarian user
+- can withdraw all funds from the library
 
-`resim new-account`
-`export pubkey=`
-`export acct=`
+```rust
+let member_badges_bucket = ResourceBuilder::new()
+    .metadata("name", "Library Membership Badge")
+    .metadata("symbol", "LMB")
+    .new_badge_fixed(member_badge_count);
+```
 
-`resim show $acct`
+- library member user
+- member count determined by parameter passed into `new`
+- can borrow, return and pay late fees on books
 
-`export xrd=`
+## Resources and Data
 
-`resim call-function --help`
-`resim call-function $package Hello new`
+```rust
+#[derive(TypeId, Encode, Decode)]
+struct Book {
+    title: String,
+    author: String,
+}
+```
 
-`export ht=`
-`export hello=`
+```rust
+#[derive(TypeId, Encode, Decode)]
+struct BorrowedBook {
+    // when the user borrowed the book
+    epoch: u64,
+    user_id: Address,
+}
+```
 
-`resim show $ht`
-`resim show $hello`
+## Methods and Functions
 
-`resim call-method $hello get_amount`
-`resim reset`
+```
+resim call-function $package Library new <member_badge_count> <membership_price> <borrow_epochs>
+```
+
+- creates a new library component
+  - `member_badge_count`: number of members allowed
+  - `membership_price`: price in XRD to join the library
+  - `borrow_epochs`: number of epochs a user can borrow a book for
+
+```
+resim call-method $lib register 1,$xrd
+```
+
+- registers a new user to the library
+- requires payment in XRD to join
+- returns the badge in a bucket
+
+```
+resim call-method $lib print_library
+```
+
+- prints the current status of the library including:
+  - current epoch (used for determing late returns)
+  - membership price and remaining badge count
+  - all books
+  - borrowed books
+
+### Member methods
+
+All the below methods require the user to pass a member's badge.
+
+Note: to make a library book require a late fee call `resim set-current-epoch <epoch>`
+
+```
+resim call-method $lib borrow_book <isbn> 1,$lmb
+```
+
+- borrows a book from the library
+  - `isbn`: book to borrow
+- internally stores the user ID and epoch of when the book was borrowed
+
+```
+resim call-method $lib return_book <isbn> 1,$lmb
+```
+
+- returns a book to the library
+  - `isbn`: book to return
+- fails if the book requires a late fee (user must cal `pay_fee` instead)
+
+```
+resim call-method $lib pay_fee <isbn> <late_fee> 1,$lmb
+```
+
+- pays for the late fee and returns the book
+  - `isbn`: book to return
+  - `late_fee`: fee to be paid in XRD
+
+### Librarian Methods
+
+The below method requires the user to pass the librarian badge.
+
+```
+resim call-method $lib withdraw_fees 1,$lb
+```
+
+- withdraws all membership and late fees from the library
--- basic/digital-library/src/lib.rs
+++ basic/digital-library/src/lib.rs
@@ -7,10 +7,17 @@ struct Book {
     author: String,
 }
 
+#[derive(TypeId, Encode, Decode)]
+struct BorrowedBook {
+    // when the user borrowed the book
+    epoch: u64,
+    user_id: Address,
+}
+
 blueprint! {
     struct Library {
         // librarian (admin) badge
-        librarian_badge: ResourceDef,
+        librarian_badge_def: ResourceDef,
 
         // holds the library funds, from late fees and memberships
         fees: Vault,
@@ -18,23 +25,25 @@ blueprint! {
         // maps ISBN numbers to books
         books: HashMap<String, Book>,
 
-        // each member receives a badge
+        // member badge
         member_badges: Vault,
         member_badge_def: ResourceDef,
-
-        // the cost of membership to the library
+        // membership cost in XRD
         membership_price: Decimal,
 
         // maps ISBN numbers to membership badge of borrower
-        borrowed_books: HashMap<String, Address>
+        borrowed_books: HashMap<String, BorrowedBook>,
+        // the number of epochs books can be borrowed for
+        borrow_epochs: u64
     }
 
     impl Library {
         // the library requires:
         // - member_badge_count: number of members allowed to join at once
         // - membership_price: the cost in XRD to join the library
+        // - loan_epochs: the number of epochs books are borrowed for
         // the librarian badge is returned to the caller
-        pub fn new(member_badge_count: u32, membership_price: Decimal) -> (Component, Bucket) {
+        pub fn new(member_badge_count: u32, membership_price: Decimal, borrow_epochs: u64) -> (Component, Bucket) {
             let librarian_badge_bucket = ResourceBuilder::new()
                 .metadata("name", "Librarian Badge")
                 .metadata("symbol", "LB")
@@ -55,13 +64,14 @@ blueprint! {
             let member_badge_def = member_badges_bucket.resource_def();
 
             let component = Self {
-                librarian_badge: librarian_badge_bucket.resource_def(),
+                librarian_badge_def: librarian_badge_bucket.resource_def(),
                 fees: Vault::new(RADIX_TOKEN),
                 books,
                 member_badges: Vault::with_bucket(member_badges_bucket),
                 member_badge_def,
                 membership_price,
-                borrowed_books: HashMap::new()
+                borrowed_books: HashMap::new(),
+                borrow_epochs: borrow_epochs
             }
             .instantiate();
 
@@ -70,14 +80,15 @@ blueprint! {
 
         // prints the state of the library
         pub fn print_library(&self) {
+            info!("Current epoch, {}", {Context::current_epoch()});
             info!("Membership price: {}, memberships available: {}", self.membership_price, self.member_badges.amount());
             info!("All books:");
-            for (key, value) in &self.books {
-                info!("{}: {}, {}", key, value.title, value.author);
+            for (isbn, book) in &self.books {
+                info!("{}: {}, {}", isbn, book.title, book.author);
             };
             info!("Borrowed books:");
-            for (key, value) in &self.borrowed_books {
-                info!("{}: {}", key, value);
+            for (isbn, borrowed_book) in &self.borrowed_books {
+                info!("{}: {}, {}", isbn, borrowed_book.user_id, borrowed_book.epoch);
             }
         }
 
@@ -91,10 +102,8 @@ blueprint! {
             // take the payment
             self.fees.put(payment);
 
-            // TODO: check for existing badge (maker_acct_addr: Address)
-
             // return the membership badge
-            info!("Successfully registered user!");
+            info!("Successfully registered user");
             self.member_badges.take(1)
         }
 
@@ -108,7 +117,9 @@ blueprint! {
             let book = self.books.get(&isbn).unwrap();
             info!("Book found (ISBN: {}, Title: {}, Author: {})", isbn, book.title, book.author);
 
-            self.borrowed_books.insert(isbn, Self::get_user_id(&auth));
+            // create and save a record of the book being borrowed
+            let borrowed_book = BorrowedBook { epoch: Context::current_epoch(), user_id: Self::get_user_id(&auth) };
+            self.borrowed_books.insert(isbn, borrowed_book);
             info!("Book borrowed")
         }
 
@@ -116,30 +127,62 @@ blueprint! {
         #[auth(member_badge_def)]
         pub fn return_book(&mut self, isbn: String) {
             info!("Attempting to return book with ISBN {}", isbn);
-            scrypto_assert!(self.borrowed_books.contains_key(&isbn), "Book not borrowed");
-            let borrower_address = self.borrowed_books.get(&isbn).unwrap();
-            let user_id = Self::get_user_id(&auth);
-            info!("{} - {}", borrower_address, user_id);
-            scrypto_assert!(borrower_address == &user_id, "Book not borrowed by this user");
+            let borrowed_book = self.get_borrowed_book(&isbn, &auth);
 
-            let book = self.books.get(&isbn).unwrap();
-            info!("Book found (ISBN: {}, Title: {}, Author: {})", isbn, book.title, book.author);
+            // check the book is not overdue
+            let book_overdue = self.is_book_overdue(borrowed_book);
+            scrypto_assert!(!book_overdue, "Book is overdue");
 
             self.borrowed_books.remove(&isbn);
             info!("Book returned")
         }
 
-        // pub fn pay_fee(&mut self, payment: Bucket)  {
-        //     info!("Attempting to pay fee with payment amount: {}", payment.amount());
-        //     // TODO: assert on amount
-        //     self.fees.put(payment) // for now pay with any amount you want, no limitation
-        // }
+        // pays the late fee and returhs the book to the library
+        #[auth(member_badge_def)]
+        pub fn pay_fee(&mut self, isbn: String, payment: Bucket)  {
+            info!("Attempting to pay fee with payment amount: {}", payment.amount());
+            let borrowed_book = self.get_borrowed_book(&isbn, &auth);
+
+            // check the book is overdue
+            let book_overdue = self.is_book_overdue(borrowed_book);
+            scrypto_assert!(book_overdue, "Book is not overdue");
+
+            // check the payment is correct
+            scrypto_assert!(payment.amount() == 1.into(), "Wrong amount sent");
+            scrypto_assert!(payment.resource_def() == RADIX_TOKEN.into(), "Can only pay with XRD");
+
+            // take the payment and remove the borrowed book record
+            self.fees.put(payment);
+            self.borrowed_books.remove(&isbn);
+            info!("Late fee paid and book returned")
+        }
 
         // time to get paid! withdraws all fees made by the library
-        #[auth(librarian_badge)]
+        #[auth(librarian_badge_def)]
         pub fn withdraw_fees(&mut self) -> Bucket {
             info!("Withdrawing all late fees: {}", self.fees.amount());
             self.fees.take_all()
+        }
+
+        // using an ISBN and user ID returns the borrowed book
+        fn get_borrowed_book(&self, isbn: &String, badge: &BucketRef) -> &BorrowedBook {
+            // check the book is borrowed and the borrower is the current user
+            scrypto_assert!(self.borrowed_books.contains_key(isbn), "Book not borrowed");
+            let borrowed_book = self.borrowed_books.get(isbn).unwrap();
+            let user_id = Self::get_user_id(&badge);
+            scrypto_assert!(borrowed_book.user_id == user_id, "Book not borrowed by this user");
+
+            let book = self.books.get(isbn).unwrap();
+            info!("Book found (ISBN: {}, Title: {}, Author: {})", isbn, book.title, book.author);
+            return borrowed_book
+        }
+
+        // returns if the given borrowed book is currently overdue
+        fn is_book_overdue(&self, borrowed_book: &BorrowedBook) -> bool {
+            // book is overdue if current epoch is past when the book was borrowed plus the allowed borrow time
+            let book_overdue = Context::current_epoch() > borrowed_book.epoch + self.borrow_epochs;
+            info!("Book borrowed on epoch {}, current epoch {}, overdue = {}", borrowed_book.epoch, Context::current_epoch(), book_overdue);
+            return book_overdue
         }
 
         // get user id from the provided badge
