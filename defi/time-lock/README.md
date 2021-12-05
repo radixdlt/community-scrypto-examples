d2d4db50820bbb2ef44fbc5d6b91aca33c80d8ab
Pavel Popov <lonelyrealist@gmail.com>
2021-12-05 06:18:50 +0300

    added badges

---
+++ defi/time-lock/README.md
@@ -0,0 +1,22 @@
+# Time Lock example
+
+How to test:
+
+## build component
+
+1. `resim reset`
+1. `export xrd=030000000000000000000000000000000000000000000000000004`
+1. `resim publish .` -> save package id into $package
+1. `resim new-account` -> save address into $acc1 and public key into $pub1
+
+Create the time lock component with 0.5% fee
+1. `resim call-function $package TimeLock new 0.5` -> save component address into $component
+1. `resim call-method $component new_user` -> save user ref into $user
+
+## lock XRD
+1. `resim call-method $component lock 1,$user 100,$xrd 100` - lock 100 XRD of user with duration added to the current epoch. Mints one TL badge with corresponding metadata -> save badge ref into $locked1
+
+## Release locked XRD
+1. `resim set-current-epoch 101` - increase current epoch to be able to release locked XRD
+1. `resim call-method $component release 1,$locked1` - release available XRD from the lock. the TL badge will be burned after 
+
---
+++ defi/time-lock/src/lib.rs
@@ -0,0 +1,119 @@
+use scrypto::prelude::*;
+
+
+blueprint! {
+    struct TimeLock {
+        /// Mint authorization to TL badges.
+        tl_minter_vault: Vault,
+        tl_minter_badge: ResourceDef,
+        // Minted tokens refs
+        minted: HashMap<Address, (Decimal, u64)>,
+
+        // Collected fees in XRD.
+        collected_fees: Vault,
+        
+        // Locked XRD
+        locked_xrd: Vault,
+
+        // fee in percents
+        fee_percent: Decimal,
+    }
+
+    impl TimeLock {
+       
+        /// Setup the Lock contract, assign an amount of XRD and the end time.
+        pub fn new(fee: Decimal) -> (Component, Bucket) {
+
+            let tl_minter_bucket = ResourceBuilder::new()
+                .metadata("name", "TL Token Mint Auth")
+                .new_badge_fixed(2);
+
+            let tl_minter_resource_def = tl_minter_bucket.resource_def();
+            let tl_minter_return_bucket: Bucket = tl_minter_bucket.take(1); // Return this badge to the caller
+            // Instantiate the Time Lock component.
+            let component = Self {
+                tl_minter_vault: Vault::with_bucket(tl_minter_bucket),
+                tl_minter_badge: tl_minter_resource_def,
+                minted: HashMap::new(),
+                collected_fees: Vault::new(RADIX_TOKEN),
+                locked_xrd: Vault::new(RADIX_TOKEN),
+                fee_percent: fee
+            }
+            .instantiate();
+
+            (component, tl_minter_return_bucket)
+        }
+
+        
+        /// Lock XRD for a certain time.
+        pub fn lock(&mut self, lock_tokens: Bucket, duration: u64) -> Bucket{
+
+            let amount = lock_tokens.amount();
+
+            scrypto_assert!(amount != Decimal::zero(), "You cannot lock zero amount");
+            scrypto_assert!(duration != 0, "You cannot lock with a zero duration");
+
+            
+            // Setup the end time.
+            let end_time = Context::current_epoch() + duration;
+
+            // fees calculation
+            let fee_amount = amount * self.fee_percent/100;
+            // setup fees to be taken from the payment
+            let fee_tokens = lock_tokens.take(fee_amount);
+
+            let available = lock_tokens.amount();
+            
+            // Put fees in collected XRD.
+            self.collected_fees.put(fee_tokens);
+            // put the rest amount of tokens to the locked vault
+
+            // Mint TL token with locked amount and end epoch as metadata
+            let tl_resource_def = ResourceBuilder::new()
+                .metadata("name", "Time lock badge")
+                .metadata("amount", available.to_string())
+                .metadata("ends", end_time.to_string())
+                .new_badge_mutable(self.tl_minter_vault.resource_def());
+
+            let tl_badge = self.tl_minter_vault.authorize(|badge| {
+                tl_resource_def
+                    .mint(1, badge)
+            });
+
+            self.minted.insert(tl_resource_def.address(), (available, end_time));
+            self.locked_xrd.put(lock_tokens);
+            tl_badge
+        }
+
+        pub fn release(&mut self, tl_badge: Bucket) -> Bucket {
+            let resource_def = tl_badge.resource_def();
+    
+            let returns = Bucket::new(RADIX_TOKEN);
+            match self.minted.get(&resource_def.address()) {
+                Some(&value) => {
+                info!("current epoch {}", Context::current_epoch());
+                scrypto_assert!(Context::current_epoch() > value.1, "Release time not yet over, wait for a bit longer"); 
+                scrypto_assert!(value.0 > Decimal::zero(), "Release amount is zero");
+
+                // Burn the TL tokens received
+                self.tl_minter_vault.authorize(|badge| {
+                    tl_badge.burn(badge);
+                });
+                // update mapping
+                self.minted.remove(&resource_def.address());
+
+                returns.put(self.locked_xrd.take(value.0));
+                },
+                _ => scrypto_abort("no mints found with provided badge")
+            }
+            
+            // Return the withdrawn tokens
+            returns
+        }
+
+        // #[auth(tl_minter_badge)]
+        pub fn claim(&mut self) -> Bucket {
+            self.collected_fees.take_all()
+        }
+    }
+}
