f513f44fa8669c8d125e9ff30be9f90f95fa2560
Markus Kahl <machisuji@gmail.com>
2021-12-23 18:30:25 +0000

    fixed demo (use NFT syntax)

--- defi/marketplace/tests/demo.ps1
+++ defi/marketplace/tests/demo.ps1
@@ -44,9 +44,8 @@ resim set-default-account $BUYER_ACC $BUYER_PUB
 
 resim call-method $XRD_MARKET market_buy $USDT 400,$XRD # BO-6, fills rest of SO-3 and most part of SO-4, SO-5 remains unfilled
 
-$BO2 = resim show $BUYER_ACC | Select-String 'resource_def: (\w+), name: "Order Ticket #2"' | %{ $_.Matches.Groups[1].Value }
-$SO3 = resim show $SELLER_ACC | Select-String 'resource_def: (\w+), name: "Order Ticket #3"' | %{ $_.Matches.Groups[1].Value }
-$BO6 = resim show $BUYER_ACC | Select-String 'resource_def: (\w+), name: "Order Ticket #6"' | %{ $_.Matches.Groups[1].Value }
+# lookup address for order ticket NFT
+$ORDER_TICKET = resim show $BUYER_ACC | Select-String 'resource_def: (\w+), name: "Order Ticket"' | %{ $_.Matches.Groups[1].Value }
 
 resim call-method $XRD_MARKET print_order_book
 
@@ -56,11 +55,12 @@ $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
 
 resim set-default-account $BUYER_ACC $BUYER_PUB
 
-resim call-method $XRD_MARKET withdraw_purchase 1,$BO2
+# "#2,$ADDR" takes the NFT with the ID 2 of the NFT with the given address from the current account's vault
+resim call-method $XRD_MARKET withdraw_purchase "#2,$ORDER_TICKET"
 
 resim set-default-account $SELLER_ACC $SELLER_PUB
 
-resim call-method $XRD_MARKET withdraw_sale 1,$SO3
+resim call-method $XRD_MARKET withdraw_sale "#3,$ORDER_TICKET"
 
 $BOUGHT_USDT = resim show $BUYER_ACC | Select-String 'amount: (\d+)\..*, name: "Tether"' | %{ $_.Matches.Groups[1].Value }
 
@@ -71,7 +71,7 @@ Write-Output ""
 if ($BOUGHT_USDT -eq "61") {
     resim set-default-account $BUYER_ACC $BUYER_PUB
 
-    resim call-method $XRD_MARKET withdraw_purchase 1,$BO6
+    resim call-method $XRD_MARKET withdraw_purchase "#6,$ORDER_TICKET"
 
     $BOUGHT_USDT = resim show $BUYER_ACC | Select-String 'amount: (\d+)\..*, name: "Tether"' | %{ $_.Matches.Groups[1].Value }
 
