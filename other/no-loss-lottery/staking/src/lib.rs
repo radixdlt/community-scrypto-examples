6f5324c528f1fb30c18d1c2ed24c6f4f99253865
Pavel Popov <lonelyrealist@gmail.com>
2021-12-23 14:37:28 +0300

    various improvements

--- other/no-loss-lottery/staking/src/lib.rs
+++ other/no-loss-lottery/staking/src/lib.rs
@@ -15,14 +15,15 @@ blueprint! {
     impl Staking {
         
         
-        pub fn new(assets: Bucket, rewards: Bucket) -> Component {
+        pub fn new(rewards: Bucket) -> Component {
             
+            let rewards_ref = rewards.resource_def();
             Self {
-                staking_pool: Vault::with_bucket(assets),
+                staking_pool: Vault::new(rewards_ref),
                 rewards_pool: Vault::with_bucket(rewards),
                 reward_value: Decimal::zero(),
                 last_update: Context::current_epoch(),
-                rate: 100.into(),
+                rate: 2.into(),
                 rewards: HashMap::new(),
                 balances: HashMap::new(),
                 paid: HashMap::new()
@@ -32,22 +33,23 @@ blueprint! {
 
         // Calculates reward per token in supply
         fn get_reward_value(&self) -> Decimal {
+
             if self.staking_pool.is_empty() {
                 debug!("Pool is empty");
                 return Decimal::zero();
             }
 
             let time = Context::current_epoch()-self.last_update;
-            debug!("Time difference: {}", time);
             let exp = 1e18 as i64;
-            let calculation = (Decimal::from(time) * self.rate * Decimal::from(exp)) / self.staking_pool.amount();
-            debug!("Period reward value: {}", calculation);
+            let exp_rate = Decimal::from(exp) / self.staking_pool.amount();
+            let epoch_rate = self.rate / 10000;
+            let calculation = Decimal::from(time) * epoch_rate * exp_rate;
             let result = self.reward_value + calculation;
-            debug!("Summarized reward value: {}", result);
             result
         }
 
         fn earned(&self, account: Address) -> Decimal {
+
             let exp = 1e18 as i64;
             let reward_value = self.get_reward_value();
 
@@ -70,9 +72,7 @@ blueprint! {
                 None => Decimal::zero()
             };
 
-            debug!("Account paid: {}", paid);
-
-            let calculation = (balance * reward_value - paid) / Decimal::from(exp);
+            let calculation = (balance * (reward_value - paid)) / Decimal::from(exp);
 
             debug!("Earned calculation: {}", calculation);
 
@@ -94,7 +94,7 @@ blueprint! {
         }
 
 
-        pub fn stake(&mut self, account: Address, bucket: Bucket) {
+        pub fn stake(&mut self, account: Address, bucket: Bucket) -> Bucket {
             self.update_reward(account);
             let amount = bucket.amount();
 
@@ -104,6 +104,8 @@ blueprint! {
             debug!("Account staking balance: {}", balance);
 
             self.staking_pool.put(bucket.take(amount));
+
+            bucket
         }
     
         pub fn withdraw(&mut self, account: Address, amount: Decimal) -> Bucket {
@@ -122,7 +124,7 @@ blueprint! {
             bucket
         }
     
-        pub fn get_reward(&mut self, account: Address) -> Bucket{
+        pub fn request_reward(&mut self, account: Address) -> Bucket{
             assert!(self.rewards.contains_key(&account), "No rewards found");            
 
             self.update_reward(account);
