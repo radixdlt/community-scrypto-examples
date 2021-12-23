f43bab7fc0d06cb777840773351a7b9ca0bb74ef
Markus Kahl <machisuji@gmail.com>
2021-12-23 18:42:27 +0000

    ps1 refactoring to allow for re-use in bash

--- defi/marketplace/tests/demo.ps1
+++ defi/marketplace/tests/demo.ps1
@@ -1,31 +1,81 @@
-# Set up environment and publish app
+# Define helper functions
 
-resim reset
-
-$BUYER_OUT = resim new-account
-$BUYER_ACC = $BUYER_OUT | Select-String 'Account address: (\w+)' | %{ $_.Matches.Groups[1].Value }
-$BUYER_PUB = $BUYER_OUT | Select-String 'Public key: (\w+)' | %{ $_.Matches.Groups[1].Value }
-
-$SELLER_OUT = resim new-account
-$SELLER_ACC = $SELLER_OUT | Select-String 'Account address: (\w+)' | %{ $_.Matches.Groups[1].Value }
-$SELLER_PUB = $SELLER_OUT | Select-String 'Public key: (\w+)' | %{ $_.Matches.Groups[1].Value }
-
-$XRD = resim show $BUYER_ACC | Select-String 'resource_def: (\w+), name: "Radix"' | %{ $_.Matches.Groups[1].Value }
-$USDT = resim new-token-fixed --name Tether --symbol USDT 1000 | Select-String 'ResourceDef: (\w+)' | %{ $_.Matches.Groups[1].Value }
-
-resim transfer 1000,$USDT $SELLER_ACC
-
-$PACKAGE = "01eb23d0867f32265935d93970aded9033cc868d31795f27d8cb62"
-
-resim publish --address $PACKAGE .
+function Save-As([String]$name, [Parameter(Mandatory = $true, ValueFromPipeline = $true)][string]$input) {
+  Set-Variable -Name $name -Value "$input" -Scope global
+}
 
 function Reload {
   resim publish --address $PACKAGE .
 }
 
+filter Get-Account-Address {
+  $_ | Select-String 'Account address: (\w+)' | %{ $_.Matches.Groups[1].Value }
+}
+
+filter Get-Public-Key {
+  $_ | Select-String 'Public key: (\w+)' | %{ $_.Matches.Groups[1].Value }
+}
+
+filter Get-Component {
+  $_ | Select-String 'Component: (\w+)' | %{$_.Matches.Groups[1].Value }
+}
+
+filter Get-Resource-Def([String]$name) {
+  $_ | Select-String "resource_def: (\w+), name: `"$name`"" | %{ $_.Matches.Groups[1].Value }
+}
+
+filter Get-Resource-Amount([String]$name) {
+  $_ | Select-String "amount: (\d+)\..*, name: `"$name`"" | %{ $_.Matches.Groups[1].Value }
+}
+
+filter Get-NFT-Def([String]$name) {
+  $_ | Select-String 'ResourceDef: (\w+)' | %{ $_.Matches.Groups[1].Value }
+}
+
+filter Get-Market-Price([String]$symbol) {
+  $_ | Select-String "$symbol \|\s*(\d\.\d+).*" | %{ $_.Matches.Groups[1].Value }
+}
+
+function Wait-For-User([String]$message) {
+  Write-Output ''
+  Write-Host -NoNewLine "$message";
+  $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+  Write-Output ''
+  Write-Output ''
+}
+
+function Exit-Unless-Equal([String]$a, [String]$b, [String]$message) {
+  if ("$a" -ne "$b") {
+    Write-Output ''
+    Write-Error $message
+    Exit
+  }
+}
+
+# Set up environment and publish app
+
+resim reset
+
+resim new-account | Save-As BUYER_OUT
+Write-Output $BUYER_OUT | Get-Account-Address | Save-As BUYER_ACC
+Write-Output $BUYER_OUT | Get-Public-Key | Save-As BUYER_PUB
+
+resim new-account | Save-As SELLER_OUT
+Write-Output $SELLER_OUT | Get-Account-Address | Save-As SELLER_ACC
+Write-Output $SELLER_OUT | Get-Public-Key | Save-As SELLER_PUB
+
+resim show $BUYER_ACC | Get-Resource-Def "Radix" | Save-As XRD
+resim new-token-fixed --name Tether --symbol USDT 1000 | Get-NFT-Def | Save-As USDT
+
+resim transfer 1000,$USDT $SELLER_ACC
+
+Write-Output "01eb23d0867f32265935d93970aded9033cc868d31795f27d8cb62" | Save-As PACKAGE
+
+resim publish --address $PACKAGE .
+
 # Create market
 
-$XRD_MARKET = resim call-function $PACKAGE Market open $XRD | Select-String 'Component: (\w+)' | %{$_.Matches.Groups[1].Value }
+resim call-function $PACKAGE Market open $XRD | Get-Component | Save-As XRD_MARKET
 
 # Create orders
 
@@ -45,13 +95,11 @@ resim set-default-account $BUYER_ACC $BUYER_PUB
 resim call-method $XRD_MARKET market_buy $USDT 400,$XRD # BO-6, fills rest of SO-3 and most part of SO-4, SO-5 remains unfilled
 
 # lookup address for order ticket NFT
-$ORDER_TICKET = resim show $BUYER_ACC | Select-String 'resource_def: (\w+), name: "Order Ticket"' | %{ $_.Matches.Groups[1].Value }
+resim show $BUYER_ACC | Get-Resource-Def "Order Ticket" | Save-As ORDER_TICKET
 
 resim call-method $XRD_MARKET print_order_book
 
-Write-Output ''
-Write-Host -NoNewLine 'Press any key to withdraw filled order ...';
-$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+Wait-For-User 'Press any key to withdraw filled order ...'
 
 resim set-default-account $BUYER_ACC $BUYER_PUB
 
@@ -62,38 +110,32 @@ resim set-default-account $SELLER_ACC $SELLER_PUB
 
 resim call-method $XRD_MARKET withdraw_sale "#3,$ORDER_TICKET"
 
-$BOUGHT_USDT = resim show $BUYER_ACC | Select-String 'amount: (\d+)\..*, name: "Tether"' | %{ $_.Matches.Groups[1].Value }
+resim show $BUYER_ACC | Get-Resource-Amount "Tether" | Save-As BOUGHT_USDT
 
 Write-Output ""
 Write-Output "Buyer bought around $BOUGHT_USDT USDT, via limit order"
 Write-Output ""
 
-if ($BOUGHT_USDT -eq "61") {
-    resim set-default-account $BUYER_ACC $BUYER_PUB
+Exit-Unless-Equal $BOUGHT_USDT "61" "Failure... (wrong limit buy amount) :("
 
-    resim call-method $XRD_MARKET withdraw_purchase "#6,$ORDER_TICKET"
+resim set-default-account $BUYER_ACC $BUYER_PUB
 
-    $BOUGHT_USDT = resim show $BUYER_ACC | Select-String 'amount: (\d+)\..*, name: "Tether"' | %{ $_.Matches.Groups[1].Value }
+resim call-method $XRD_MARKET withdraw_purchase "#6,$ORDER_TICKET"
 
-    Write-Output ""
-    Write-Output "Buyer bought another 122 USDT ($BOUGHT_USDT altogether) roughly, via market order"
+resim show $BUYER_ACC | Get-Resource-Amount "Tether" | Save-As BOUGHT_USDT
 
-    if ($BOUGHT_USDT -eq "183") {
-        $MARKET_PRICE = resim call-method $XRD_MARKET print_market_prices | Select-String 'USDT \|\s*(\d\.\d+).*' | %{ $_.Matches.Groups[1].Value }
+Write-Output ""
+Write-Output "Buyer bought another 122 USDT ($BOUGHT_USDT altogether) roughly, via market order"
 
-        if ($MARKET_PRICE -eq "3.29") {
-            Write-Output ""
-            Write-Output "Market price is 3.29 as last sell order filled was SO-4"
-            Write-Output ""
-            Write-Output "Success! :)"
-        } else {
-          Write-Output "Failure... (wrong market price) :("
-        }
-    } else {
-      Write-Output "Failure... (wrong market buy amount) :("
-    }
-} else {
-    Write-Output "Failure... (wrong limit buy amount) :("
-}
+Exit-Unless-Equal $BOUGHT_USDT "183" "Failure... (wrong market buy amount) :("
+
+resim call-method $XRD_MARKET print_market_prices | Get-Market-Price "USDT" | Save-As MARKET_PRICE
+
+Exit-Unless-Equal $MARKET_PRICE "3.29" "Failure... (wrong market price) :("
+
+Write-Output ""
+Write-Output "Market price is 3.29 as last sell order filled was SO-4"
+Write-Output ""
+Write-Output "Success! :)"
 
 Write-Output ""
