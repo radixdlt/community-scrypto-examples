beb2b7579214edadd35ad786d9d31893b78e9a66
Patrick Gerard <patrick.gerard@maven360.io>
2022-04-16 15:18:28 +0100

    delete sample comments

--- basic/bank/src/lib.rs
+++ basic/bank/src/lib.rs
@@ -2,19 +2,13 @@ use scrypto::prelude::*;
 
 blueprint! {
     struct Bank {
-        // Define what resources and data will be managed by Hello components
         bank_account: Vault,
         cash: Vault,
         owner_badge: ResourceDef,
-
     }
 
     impl Bank {
-        // Implement the functions and methods which will manage those resources and data
-        
-        // This is a function, and can be called directly on the blueprint once deployed
         pub fn start() -> (Component, Bucket) {
-            // Create a new token called "HelloToken," with a fixed supply of 1000, and put that supply into a bucket
             let mut initial_cash: Bucket = ResourceBuilder::new_fungible(DIVISIBILITY_MAXIMUM)
                 .metadata("description", "USD")
                 .initial_supply_fungible(1000);
@@ -35,13 +29,9 @@ blueprint! {
             (instantiate_accounts, owner_badge)
         }
 
-        // This is a method, because it needs a reference to self.  Methods can only be called on components
         pub fn balances(&self) {
             info!("My bank balance is: {}", self.bank_account.amount());
             info!("My cash balance is: {}", self.cash.amount());
-            // If the semi-colon is omitted on the last line, the last value seen is automatically returned
-            // In this case, a bucket containing 1 HelloToken is returned
-            // self.sample_vault.take(1)
         }
 
         pub fn bank_deposit(&mut self, exchange: Decimal) {
