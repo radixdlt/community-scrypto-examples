c3f57a7dce6bac11be2f3bf760440a363288ceff
Clement <clement@radixdlt.com>
2022-04-27 16:22:26 -0400

    Migrated toys/accumulator

--- toys/accumulator/tests/lib.rs
+++ toys/accumulator/tests/lib.rs
@@ -7,53 +7,73 @@ fn test_accumulating_vault() {
     // Set up environment.
     let mut ledger = InMemorySubstateStore::with_bootstrap();
     let mut executor = TransactionExecutor::new(&mut ledger, false);
-    let key = executor.new_public_key();
-    let account = executor.new_account(key);
-    let package = executor.publish_package(include_code!("scryptoAccumulator")).unwrap();
+    let (pk, sk, account) = executor.new_account();
+    let package = executor.publish_package(compile_package!()).unwrap();
     
-    executor.ledger_mut().set_epoch(0);
+    executor.substate_store_mut().set_epoch(0);
 
     // Test the `new` function.
-    let transaction1 = TransactionBuilder::new(&executor)
-        .call_function(package, "AccumulatingVault", "new", vec!["1.0".to_string()], None)
+    let transaction1 = TransactionBuilder::new()
+        .call_function(package, "AccumulatingVault", "new", args![Decimal::one()])
         .call_method_with_all_resources(account, "deposit_batch")
-        .build(vec![key])
-        .unwrap();
-    let receipt1 = executor.run(transaction1).unwrap();
+        .build(executor.get_nonce(vec![pk]))
+        .sign([&sk]);
+
+    let receipt1 = executor.validate_and_execute(&transaction1).unwrap();
     println!("{:?}\n", receipt1);
     assert!(receipt1.result.is_ok());
 
     // Test the `withdraw` method with 0.
-    let component = receipt1.component(0).unwrap();
-    let badge = receipt1.resource_def(1).unwrap();
-    let transaction2 = TransactionBuilder::new(&executor)
-        .call_method(component, "withdraw", vec!["0.0".to_string(), format!("1,{}", badge)], Some(account))
+    let component = receipt1.new_component_addresses.get(0).unwrap();
+    let badge = receipt1.new_resource_addresses.get(1).unwrap();
+    let transaction2 = TransactionBuilder::new()
+        .withdraw_from_account(*badge, account)
+        .take_from_worktop(*badge, |builder, bucket_id| {
+            builder.create_proof_from_bucket(bucket_id, |builder, proof_id| {
+                builder.push_to_auth_zone(proof_id)
+            })
+        })
+        .call_method(*component, "withdraw", args![Decimal::zero()])
         .call_method_with_all_resources(account, "deposit_batch")
-        .build(vec![key])
-        .unwrap();
-    let receipt2 = executor.run(transaction2).unwrap();
+        .build(executor.get_nonce(vec![pk]))
+        .sign([&sk]);
+
+    let receipt2 = executor.validate_and_execute(&transaction2).unwrap();
     println!("{:?}\n", receipt2);
     assert!(receipt2.result.is_ok());
     
     // Test the `withdraw` method with 5.
-    let transaction3 = TransactionBuilder::new(&executor)
-        .call_method(component, "withdraw", vec!["5.0".to_string(), format!("1,{}", badge)], Some(account))
+    let transaction3 = TransactionBuilder::new()
+        .withdraw_from_account(*badge, account)
+        .take_from_worktop(*badge, |builder, bucket_id| {
+            builder.create_proof_from_bucket(bucket_id, |builder, proof_id| {
+                builder.push_to_auth_zone(proof_id)
+            })
+        })
+        .call_method(*component, "withdraw", args![dec!("5.0")])
         .call_method_with_all_resources(account, "deposit_batch")
-        .build(vec![key])
-        .unwrap();
-    let receipt3 = executor.run(transaction3).unwrap();
+        .build(executor.get_nonce(vec![pk]))
+        .sign([&sk]);
+
+    let receipt3 = executor.validate_and_execute(&transaction3).unwrap();
     println!("{:?}\n", receipt3);
     assert!(!receipt3.result.is_ok());
     
-    executor.ledger_mut().set_epoch(10);
+    executor.substate_store_mut().set_epoch(10);
     
     // Test the `withdraw` method with 5.
-    let transaction4 = TransactionBuilder::new(&executor)
-        .call_method(component, "withdraw", vec!["5.0".to_string(), format!("1,{}", badge)], Some(account))
+    let transaction4 = TransactionBuilder::new()
+        .withdraw_from_account(*badge, account)
+        .take_from_worktop(*badge, |builder, bucket_id| {
+            builder.create_proof_from_bucket(bucket_id, |builder, proof_id| {
+                builder.push_to_auth_zone(proof_id)
+            })
+        })
+        .call_method(*component, "withdraw", args![dec!("5.0")])
         .call_method_with_all_resources(account, "deposit_batch")
-        .build(vec![key])
-        .unwrap();
-    let receipt4 = executor.run(transaction4).unwrap();
+        .build(executor.get_nonce([pk]))
+        .sign([&sk]);
+    let receipt4 = executor.validate_and_execute(&transaction4).unwrap();
     println!("{:?}\n", receipt4);
     assert!(receipt4.result.is_ok());
 }
