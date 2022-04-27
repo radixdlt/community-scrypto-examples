c1fad3bcf3ace4a250ea870350633a3068936e04
0xOmarA <OmarAbdulla7@hotmail.com>
2022-04-28 01:19:44 +0300

    Updated basic/auction to v0.4.0

--- basic/auction/src/lib.rs
+++ basic/auction/src/lib.rs
@@ -3,7 +3,7 @@ use scrypto::prelude::*;
 
 const PAYMENT_DEADLINE: u64 = 100;
 
-#[derive(TypeId, Encode, Decode)]
+#[derive(TypeId, Encode, Decode, Describe)]
 struct Bidder {
     bid: Decimal,
     bid_bond_reclaimed: bool,
@@ -11,99 +11,106 @@ struct Bidder {
 
 blueprint! {
     struct Auction {
-        component_address: Address,
-
         offering: Vault,
         bid_bonds: Vault,
         payment: Vault,
 
         start: u64,
         duration: u64,
-        payment_resource: ResourceDef,
+        payment_resource: ResourceAddress,
         reserve_price: Decimal,
         bid_bond: Decimal,
         
-        bidders: LazyMap<ResourceDef, Bidder>,
+        bidders: LazyMap<ResourceAddress, Bidder>,
         highest_bid: Decimal,
         
-        auctioneer_badge: ResourceDef,
+        auctioneer_badge: ResourceAddress,
         payment_claimed: bool,
     }
 
     impl Auction {
-        pub fn new(offering: Bucket, duration: u64, payment_resource: Address, reserve_price: Decimal, bid_bond: Decimal) -> (Component, Bucket) {
+        pub fn new(
+            offering: Bucket, 
+            duration: u64, 
+            payment_resource: ResourceAddress, 
+            reserve_price: Decimal, 
+            bid_bond: Decimal
+        ) -> (ComponentAddress, Bucket) {
             assert!(offering.amount() > Decimal::zero(), "Incorrect offering");
             
             assert!(bid_bond <= reserve_price, "Bid bond higher than the reserve price");
 
+            // mint the auctioneer badge
+            let auctioneer_badge = ResourceBuilder::new_fungible()
+                .divisibility(DIVISIBILITY_NONE)
+                .metadata("name", "Acutioneer badge")
+                .initial_supply(1);
+
+            // Setting the access rules for the component
+            let access_rules = AccessRules::new()
+                .method("claim_payment", rule!(require(auctioneer_badge.resource_address())))
+                .default(rule!(allow_all));
+
             // instantiate an auction component
             let auction = Self {
-                component_address: Address::Component([0; 26]),
-                
                 offering: Vault::with_bucket(offering),
                 bid_bonds: Vault::new(payment_resource.clone()),
                 payment: Vault::new(payment_resource.clone()),
 
-                start: Context::current_epoch(),
+                start: Runtime::current_epoch(),
                 duration,
-                payment_resource: ResourceDef::from(payment_resource),
+                payment_resource: ResourceAddress::from(payment_resource),
                 reserve_price,
                 bid_bond,
 
                 bidders: LazyMap::new(),
                 highest_bid: Decimal::zero(),
 
-                auctioneer_badge: ResourceDef::from(Address::ResourceDef([0; 26])),
+                auctioneer_badge: ResourceAddress([0; 26]),
                 payment_claimed: false,
             }
-            .instantiate();
-
-            // save the component address (it doesn't seem like there is a way to get it later from iside a method)
-            let mut auction_state = auction.get_state::<Self>();
-            auction_state.component_address = auction.address();
-
-            // mint the auctioneer badge
-            let auctioneer_badge = ResourceBuilder::new_fungible(DIVISIBILITY_NONE)
-                .metadata("name", "Acutioneer badge")
-                .metadata("auction", auction_state.component_address.to_string())
-                .initial_supply_fungible(1);
-
-            // save the auctioner badge resource definition
-            auction_state.auctioneer_badge = auctioneer_badge.resource_def();
-            auction.put_state(auction_state);
+            .instantiate()
+            .add_access_check(access_rules)
+            .globalize();
 
             (auction, auctioneer_badge)
         }
 
         pub fn register(&mut self, bid_bond: Bucket) -> Bucket {
             // check if the auction is open
-            assert!(Context::current_epoch() <= self.start + self.duration, "Auction closed");
+            assert!(Runtime::current_epoch() <= self.start + self.duration, "Auction closed");
 
             // check the bid bond
-            assert!(bid_bond.resource_def() == self.payment_resource, "Incorrect payment token");
+            assert!(bid_bond.resource_address() == self.payment_resource, "Incorrect payment token");
             assert!(bid_bond.amount() == self.bid_bond, "Incorrect bid bond");
 
             // take the bid bond
             self.bid_bonds.put(bid_bond);
 
             // mint a bidder badge
-            let bidder_badge = ResourceBuilder::new_fungible(DIVISIBILITY_NONE)
+            let bidder_badge = ResourceBuilder::new_fungible()
+                .divisibility(DIVISIBILITY_NONE)
                 .metadata("name", "Bidder badge")
-                .metadata("auction", self.component_address.to_string())
-                .initial_supply_fungible(1);
+                .initial_supply(1);
 
             // save the bidder using the bidder badge resource definition
-            self.bidders.insert(bidder_badge.resource_def(), Bidder { bid: Decimal::zero(), bid_bond_reclaimed: false });
+            self.bidders.insert(
+                bidder_badge.resource_address(), 
+                Bidder { 
+                    bid: Decimal::zero(), 
+                    bid_bond_reclaimed: false 
+                }
+            );
 
             bidder_badge
         }
 
-        pub fn bid(&mut self, bid: Decimal, bidder_badge: BucketRef) {
+        pub fn bid(&mut self, bid: Decimal, bidder_badge: Proof) {
             // check if the auction is open
-            assert!(Context::current_epoch() <= self.start + self.duration, "Auction closed");
+            assert!(Runtime::current_epoch() <= self.start + self.duration, "Auction closed");
 
             // check the bidder badge and get the bidder
-            let bidder_id = bidder_badge.resource_def();
+            let bidder_id = bidder_badge.resource_address();
             let mut bidder = self.get_bidder(bidder_badge);
 
             // check the bid
@@ -116,12 +123,12 @@ blueprint! {
             self.highest_bid = bid;
         }
 
-        pub fn claim_offering(&mut self, payment: Bucket, bidder_badge: BucketRef) -> Bucket {
+        pub fn claim_offering(&mut self, payment: Bucket, bidder_badge: Proof) -> Bucket {
             // check if the auction is closed
-            assert!(Context::current_epoch() > self.start + self.duration, "Auction open");
+            assert!(Runtime::current_epoch() > self.start + self.duration, "Auction open");
 
             // check if the payment deadline has not passed yet
-            assert!(Context::current_epoch() <= self.start + self.duration + PAYMENT_DEADLINE, "Payment deadline passed");
+            assert!(Runtime::current_epoch() <= self.start + self.duration + PAYMENT_DEADLINE, "Payment deadline passed");
 
             // check the bidder badge and get the bidder
             let bidder = self.get_bidder(bidder_badge);
@@ -133,7 +140,7 @@ blueprint! {
             assert!(!self.offering.is_empty(), "Offering already claimed");
 
             // check the payment
-            assert!(payment.resource_def() == self.payment_resource, "Incorrect payment token");
+            assert!(payment.resource_address() == self.payment_resource, "Incorrect payment token");
             assert!(payment.amount() == self.highest_bid - self.bid_bond, "Incorrect payment amount");
 
             // take the payment and return the offering
@@ -142,12 +149,12 @@ blueprint! {
             self.offering.take_all()
         }
 
-        pub fn reclaim_bid_bond(&mut self, bidder_badge: BucketRef) -> Bucket {
+        pub fn reclaim_bid_bond(&mut self, bidder_badge: Proof) -> Bucket {
             // check if auction is closed
-            assert!(Context::current_epoch() > self.start + self.duration, "Acution open");
+            assert!(Runtime::current_epoch() > self.start + self.duration, "Acution open");
 
             // check bidder badge and get the bidder
-            let bidder_id = bidder_badge.resource_def();
+            let bidder_id = bidder_badge.resource_address();
             let mut bidder = self.get_bidder(bidder_badge);
 
             // check if it is not the winning bid
@@ -163,10 +170,9 @@ blueprint! {
             self.bid_bonds.take(self.bid_bond)
         }
         
-        #[auth(auctioneer_badge)]
         pub fn claim_payment(&mut self) -> (Bucket, Bucket) {
             // check if auction is closed
-            assert!(Context::current_epoch() > self.start + self.duration, "Auction open");
+            assert!(Runtime::current_epoch() > self.start + self.duration, "Auction open");
 
             // check if the payment has not been yet claimed
             assert!(!self.payment_claimed, "Payment already claimed");
@@ -175,7 +181,7 @@ blueprint! {
             assert!(
                 self.highest_bid == Decimal::zero() ||
                 !self.payment.is_empty() || 
-                Context::current_epoch() > self.start + self.duration + PAYMENT_DEADLINE,
+                Runtime::current_epoch() > self.start + self.duration + PAYMENT_DEADLINE,
                 "Payment not received and the payment deadline not passed");
 
             // if the offering has been sold then add the winner's bid bond to the payment
@@ -190,9 +196,9 @@ blueprint! {
             (self.payment.take_all(), self.offering.take_all())            
         }
 
-        fn get_bidder(&self, bidder_badge: BucketRef) -> Bidder {
+        fn get_bidder(&self, bidder_badge: Proof) -> Bidder {
             assert!(bidder_badge.amount() > Decimal::zero(), "No bidder badge presented");
-            let bidder = self.bidders.get(&bidder_badge.resource_def());
+            let bidder = self.bidders.get(&bidder_badge.resource_address());
             assert!(bidder.is_some(), "Incorrect bidder badge");
             bidder_badge.drop();
 
