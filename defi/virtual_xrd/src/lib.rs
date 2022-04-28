831fbc4ff5362864abbb9d58fece6fa33062c5f8
Clement <clement@radixdlt.com>
2022-04-28 17:06:45 -0400

    Migrated defi/virtual_xrd

--- defi/virtual_xrd/src/lib.rs
+++ defi/virtual_xrd/src/lib.rs
@@ -2,22 +2,23 @@ use scrypto::prelude::*;
 
 blueprint! {
     struct VirtualXrd {
-        vxrd_resource_def: ResourceDef,
+        vxrd_resource_def: ResourceAddress,
         xrd_vault: Vault,
         minter_badge: Vault
     }
 
     impl VirtualXrd {
-        pub fn new() -> Component {
-            let minter_badge = ResourceBuilder::new_fungible(DIVISIBILITY_NONE)
+        pub fn new() -> ComponentAddress {
+            let minter_badge = ResourceBuilder::new_fungible()
+                .divisibility(DIVISIBILITY_NONE)
                 .metadata("name", "Minter Badge")
-                .initial_supply_fungible(1);
+                .initial_supply(1);
 
-            let vxrd_resource_def = ResourceBuilder::new_fungible(DIVISIBILITY_MAXIMUM)
+            let vxrd_resource_def = ResourceBuilder::new_fungible()
                 .metadata("name", "VXRD")
                 .metadata("symbol", "VXRD")
-                .flags(MINTABLE | BURNABLE)
-                .badge(minter_badge.resource_def(), MAY_MINT | MAY_BURN)
+                .mintable(rule!(require(minter_badge.resource_address())), LOCKED)
+                .burnable(rule!(require(minter_badge.resource_address())), LOCKED)
                 .no_initial_supply();
 
             Self {
@@ -25,7 +26,7 @@ blueprint! {
                 xrd_vault: Vault::new(RADIX_TOKEN),
                 minter_badge: Vault::with_bucket(minter_badge)
             }
-            .instantiate()
+            .instantiate().globalize()
         }
 
         pub fn swap_xrd_for_vxrd(&mut self, xrd: Bucket) -> Bucket {
@@ -34,21 +35,21 @@ blueprint! {
 
             let amount = xrd.amount();
             self.xrd_vault.put(xrd);
-            let vxrd_tokens = self.minter_badge.authorize(|badge| {
-                self.vxrd_resource_def.mint(amount, badge)
+            let vxrd_tokens = self.minter_badge.authorize(|| {
+                borrow_resource_manager!(self.vxrd_resource_def).mint(amount)
             });
 
             vxrd_tokens
         }
 
         pub fn swap_vxrd_for_xrd(&mut self, vxrd: Bucket) -> Bucket {
-            assert!(vxrd.resource_address() == self.vxrd_resource_def.address(),
+            assert!(vxrd.resource_address() == self.vxrd_resource_def,
                 "The tokens for the opportunity must be VXRD");
 
             let amount = vxrd.amount();
             let xrd_tokens = self.xrd_vault.take(amount);
-            self.minter_badge.authorize(|badge| {
-                vxrd.burn_with_auth(badge);
+            self.minter_badge.authorize(|| {
+                vxrd.burn();
             });
 
             xrd_tokens
