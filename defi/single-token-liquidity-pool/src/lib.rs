6aebe524e7041bc1eab2e61ec69da7bc42927807
Clement <clement@radixdlt.com>
2022-04-28 17:28:42 -0400

    Migrated defi/single-token-liquidity-pool

--- defi/single-token-liquidity-pool/src/lib.rs
+++ defi/single-token-liquidity-pool/src/lib.rs
@@ -10,27 +10,30 @@ blueprint! {
         pool: Vault,
         fees: Vault,
         lp_minter_badge: Vault,
-        lp_token: ResourceDef
+        lp_token: ResourceAddress
     }
 
     impl SingleTokenLiquidityPool {
-        pub fn new(initial_funds: Bucket, lp_initial_supply: Decimal) -> Component {
-            let funds_resource_def = initial_funds.resource_def();
+        pub fn new(initial_funds: Bucket, lp_initial_supply: Decimal) -> ComponentAddress {
+            let funds_resource_def = initial_funds.resource_address();
 
             // Create badge that will be used to mint and burn LP tokens
-            let lp_minter: Bucket = ResourceBuilder::new_fungible(DIVISIBILITY_NONE)
-                .initial_supply_fungible(1);
+            let lp_minter: Bucket = ResourceBuilder::new_fungible()
+                .divisibility(DIVISIBILITY_NONE)
+                .initial_supply(1);
 
             // Create the LP token definition
-            let mut lp_token = ResourceBuilder::new_fungible(DIVISIBILITY_MAXIMUM)
+            let mut lp_token = ResourceBuilder::new_fungible()
                 .metadata("name", "LP Token")
                 .metadata("symbol", "LP")
-                .flags(MINTABLE | BURNABLE)
-                .badge(lp_minter.resource_def(), MAY_MINT | MAY_BURN)
+                .mintable(rule!(require(lp_minter.resource_address())), LOCKED)
+                .burnable(rule!(require(lp_minter.resource_address())), LOCKED)
                 .no_initial_supply();
 
             // Mint initial LP tokens
-            lp_token.mint(lp_initial_supply, lp_minter.present());
+            lp_minter.authorize(|| {
+                borrow_resource_manager!(lp_token).mint(lp_initial_supply);
+            });
 
             Self {
                 pool: Vault::with_bucket(initial_funds),
@@ -39,7 +42,7 @@ blueprint! {
                 lp_token: lp_token
 
             }
-            .instantiate()
+            .instantiate().globalize()
         }
 
         // Will be called by other components
@@ -53,21 +56,21 @@ blueprint! {
             self.pool.put(liquidity);
 
             // Return newly minted lp_tokens
-            self.lp_minter_badge.authorize(|badge| {
-                self.lp_token
-                    .mint(self.lp_token.total_supply() * pool_share, badge)
+            self.lp_minter_badge.authorize(|| {
+                borrow_resource_manager!(self.lp_token)
+                    .mint(borrow_resource_manager!(self.lp_token).total_supply() * pool_share)
             })
         }
 
         // Give LP token back to get portion of the pool and fees
         pub fn remove_liquidity(&mut self, lp_tokens: Bucket) -> Bucket {
-            assert!(lp_tokens.resource_def() == self.lp_token, "Wrong LP token !");
+            assert!(lp_tokens.resource_address() == self.lp_token, "Wrong LP token !");
 
-            let share = lp_tokens.amount() / self.lp_token.total_supply();
+            let share = lp_tokens.amount() / borrow_resource_manager!(self.lp_token).total_supply();
             
             // Burn the provided LP token
-            self.lp_minter_badge.authorize(|badge| {
-                lp_tokens.burn_with_auth(badge);
+            self.lp_minter_badge.authorize(|| {
+                lp_tokens.burn();
             });
 
             // Return the share of fees and pool
