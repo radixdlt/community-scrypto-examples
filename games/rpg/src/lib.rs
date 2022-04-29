641c440cb1e4e88e4920f1c4e02bfe018a7fd119
Ethan Tran <ethantran.et77@gmail.com>
2022-04-29 09:03:53 -0700

    Update lib.rs

--- games/rpg/src/lib.rs
+++ games/rpg/src/lib.rs
@@ -1,6 +1,25 @@
 use scrypto::prelude::*;
+use sbor::*;
 
+#[derive(TypeId, Encode, Decode, Describe)]
+pub enum Class {
+    Gladiator,
+    Rogue,
+    Ranger,
+    Mystic,
+    Paladin,
+    Enchanter,
+}
 
+#[derive(NonFungibleData)]
+pub struct Account {
+    #[scrypto(mutable)]
+    exp: Decimal,
+    class: Class,
+    level: u8,
 
 blueprint! {
+    struct Substradix {
+         gold_vault: Vault,
+         
 
