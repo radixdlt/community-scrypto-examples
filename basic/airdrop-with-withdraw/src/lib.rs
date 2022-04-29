27785cb884cfafad482b148f0495ada836b98472
0xOmarA <OmarAbdulla7@hotmail.com>
2022-04-29 03:27:42 +0300

    Updated the basic/airdrop-with-withdraw example

--- basic/airdrop-with-withdraw/src/lib.rs
+++ basic/airdrop-with-withdraw/src/lib.rs
@@ -3,97 +3,103 @@ use scrypto::prelude::*;
 #[derive(NonFungibleData)]
 pub struct AirdropWithWithdrawData {
     amount: Decimal,
-    token_type : Address, 
+    token_type: ResourceAddress,
     #[scrypto(mutable)]
-    is_collected : bool
+    is_collected: bool,
 }
 
 blueprint! {
-
-    
     struct AirdropWithWithdraw {
-        admin_badge: Address,
-        tokens : Vault,
-        recipient_badge_def : ResourceDef,
-        minter_badge_vault : Vault
+        admin_badge: ResourceAddress,
+        tokens: Vault,
+        recipient_badge_address: ResourceAddress,
+        minter_badge_vault: Vault,
     }
 
     impl AirdropWithWithdraw {
-        
-        pub fn new(token_type : Address) -> (Component, Bucket) {
+        pub fn new(token_type: ResourceAddress) -> (ComponentAddress, Bucket) {
+            let admin_badge = ResourceBuilder::new_fungible()
+                .divisibility(DIVISIBILITY_NONE)
+                .initial_supply(1);
 
-            let admin_badge = ResourceBuilder::new_fungible(DIVISIBILITY_NONE)
-                                .initial_supply_fungible(1);
+            let minter_badge = ResourceBuilder::new_fungible()
+                .divisibility(DIVISIBILITY_NONE)
+                .metadata("name", "minter badge")
+                .initial_supply(1);
 
-            let minter_badge = ResourceBuilder::new_fungible(DIVISIBILITY_NONE)
-                                               .metadata("name", "minter badge")
-                                               .initial_supply_fungible(1); 
+            let recipient_badge_address = ResourceBuilder::new_non_fungible()
+                .metadata("name", "recipient badge")
+                .mintable(rule!(require(minter_badge.resource_address())), LOCKED)
+                .updateable_non_fungible_data(rule!(require(minter_badge.resource_address())), LOCKED)
+                .no_initial_supply();
 
-            let recipient_badge_def = ResourceBuilder::new_non_fungible()
-                                .metadata("name", "recipient badge")
-                                .flags(MINTABLE | INDIVIDUAL_METADATA_MUTABLE)
-                                .badge(minter_badge.resource_address(), MAY_MINT | MAY_CHANGE_INDIVIDUAL_METADATA)
-                                .no_initial_supply(); 
+            let access_rules = AccessRules::new()
+                .method("add_recipient", rule!(require(admin_badge.resource_address())))
+                .default(rule!(allow_all));
 
-            
-            
             let component = Self {
-                admin_badge : admin_badge.resource_address(),
-                tokens : Vault::new(token_type),
-                recipient_badge_def ,
-                minter_badge_vault : Vault::with_bucket(minter_badge)
+                admin_badge: admin_badge.resource_address(),
+                tokens: Vault::new(token_type),
+                recipient_badge_address,
+                minter_badge_vault: Vault::with_bucket(minter_badge),
             }
-            .instantiate();
+            .instantiate()
+            .add_access_check(access_rules)
+            .globalize();
 
-            return (component, admin_badge)
+            return (component, admin_badge);
         }
 
-
-        #[auth(admin_badge)]
-        pub fn add_recipient(&mut self, recipient: Address, tokens : Bucket)  {
-
-            assert!(tokens.amount() > Decimal::zero(), "tokens quantity cannot be 0"); 
-            assert_eq!(tokens.resource_address(), self.tokens.resource_address(), "token address must match"); 
-
-            let recipient_badge_id = Uuid::generate();
-            let recipient_badge = self.minter_badge_vault.authorize(|auth|
-                return self.recipient_badge_def.mint_non_fungible(&NonFungibleKey::from(recipient_badge_id) , 
-                                                         AirdropWithWithdrawData {  
-                                                                                    amount : tokens.amount() , 
-                                                                                    token_type : tokens.resource_address(),
-                                                                                    is_collected : false
-                                                                                }, 
-                                                         auth)
+        pub fn add_recipient(&mut self, recipient: ComponentAddress, tokens: Bucket) {
+            assert!(
+                tokens.amount() > Decimal::zero(),
+                "tokens quantity cannot be 0"
             );
+            assert_eq!(
+                tokens.resource_address(),
+                self.tokens.resource_address(),
+                "token address must match"
+            );
+
+            let recipient_badge = self.minter_badge_vault.authorize(|| {
+                borrow_resource_manager!(self.recipient_badge_address).mint_non_fungible(
+                    &NonFungibleId::random(),
+                    AirdropWithWithdrawData {
+                        amount: tokens.amount(),
+                        token_type: tokens.resource_address(),
+                        is_collected: false,
+                    },
+                )
+            });
             self.tokens.put(tokens);
 
-            Component::from(recipient).call::<()>("deposit", vec![scrypto_encode(&recipient_badge)]);
+            borrow_component!(recipient).call::<()>("deposit", args![recipient_badge]);
         }
-        
 
-        #[auth(recipient_badge_def)]
-        pub fn available_token(& self) -> Decimal
-        {
-            let recipient_badge_id  = auth.get_non_fungible_key();  
-            let  nft_data : AirdropWithWithdrawData = self.recipient_badge_def.get_non_fungible_data(&recipient_badge_id);
-            let mut result : Decimal = Decimal::zero();
+        pub fn available_token(&self, auth: Proof) -> Decimal {
+            assert_eq!(auth.resource_address(), self.recipient_badge_address, "Invalid Badge Provided");
+            assert_eq!(auth.amount(), dec!("1"), "Invalid Badge Provided");
+
+            let nft_data = auth.non_fungible::<AirdropWithWithdrawData>().data();
+            let mut result: Decimal = Decimal::zero();
             if !nft_data.is_collected {
-                result = nft_data.amount; 
+                result = nft_data.amount;
             }
             info!("available : {}", result);
-            return result; 
+            return result;
         }
 
-        #[auth(recipient_badge_def)]
-        pub fn withdraw_token(&mut self) -> Bucket
-        {  
-            let recipient_badge_id  = auth.get_non_fungible_key();  
-            let mut nft_data : AirdropWithWithdrawData = self.recipient_badge_def.get_non_fungible_data(&recipient_badge_id);
-            assert!(!nft_data.is_collected, "withdraw already done"); 
-            nft_data.is_collected = true; 
-            let amount = nft_data.amount; 
-            self.minter_badge_vault.authorize({
-                |auth| self.recipient_badge_def.update_non_fungible_data(&recipient_badge_id, nft_data, auth)
+        pub fn withdraw_token(&mut self, auth: Proof) -> Bucket {
+            assert_eq!(auth.resource_address(), self.recipient_badge_address, "Invalid Badge Provided");
+            assert_eq!(auth.amount(), dec!("1"), "Invalid Badge Provided");
+
+            let mut nft_data = auth.non_fungible::<AirdropWithWithdrawData>().data();
+            assert!(!nft_data.is_collected, "withdraw already done");
+            nft_data.is_collected = true;
+            let amount = nft_data.amount;
+            self.minter_badge_vault.authorize({|| {
+                auth.non_fungible().update_data(nft_data);
+                }
             });
             info!("withdraw_token : {}", amount);
             return self.tokens.take(amount);
--- basic/airdrop-with-withdraw/tests/lib.rs
+++ basic/airdrop-with-withdraw/tests/lib.rs
@@ -1,190 +1,243 @@
-use radix_engine::ledger::*;
-use radix_engine::transaction::*;
-use scrypto::prelude::*;
 
+// TODO: Update the tests for this example when the get balance is updated for the account component.
 
-#[test]
-fn try_withdraw_without_added_recipients_must_be_failed() {
-    // Set up environment.
+// use radix_engine::ledger::*;
+// use radix_engine::transaction::*;
+// use scrypto::prelude::*;
 
-    let mut ledger = InMemorySubstateStore::with_bootstrap();
-    let mut test_env = TestEnv::new(&mut ledger, RADIX_TOKEN);
-    assert_eq!(test_env.get_balance(test_env.admin_account, RADIX_TOKEN).unwrap(), dec!(1000000));
+// #[test]
+// fn try_withdraw_without_added_recipients_must_be_failed() {
+//     // Set up environment.
 
-    // withdraw_token    
-    let  (not_recipient_key, not_recipient_address)  = test_env.new_account();
-    let not_withdraw_receipt =  test_env.withdraw_token(not_recipient_key, not_recipient_address);
-    assert!(!not_withdraw_receipt.success);
-    let log_message = &not_withdraw_receipt.logs.get(0).unwrap().1;
-    assert!(log_message.starts_with("Panicked at 'Insufficient balance'"));
-}
+//     let mut ledger = InMemorySubstateStore::with_bootstrap();
+//     let mut test_env = TestEnv::new(&mut ledger, RADIX_TOKEN);
+//     assert_eq!(
+//         test_env
+//             .get_balance(test_env.admin_account, RADIX_TOKEN)
+//             .unwrap(),
+//         dec!(1000000)
+//     );
 
-#[test]
-fn try_withdraw_already_done_must_be_failed() {
-    // Set up environment.
+//     // withdraw_token
+//     let (not_recipient_key, not_recipient_address) = test_env.new_account();
+//     let not_withdraw_receipt = test_env.withdraw_token(not_recipient_key, not_recipient_address);
+//     assert!(!not_withdraw_receipt.success);
+//     let log_message = &not_withdraw_receipt.logs.get(0).unwrap().1;
+//     assert!(log_message.starts_with("Panicked at 'Insufficient balance'"));
+// }
 
-    let mut ledger = InMemorySubstateStore::with_bootstrap();
-    let mut test_env = TestEnv::new(&mut ledger, RADIX_TOKEN);
-    assert_eq!(test_env.get_balance(test_env.admin_account, RADIX_TOKEN).unwrap(), dec!(1000000));
-    let  (not_recipient_key, not_recipient_address)  = test_env.new_account();
-    let token_by_recipient : Decimal =  dec!(100); 
-    // addRecipient
-    test_env.add_recipient(not_recipient_address, RADIX_TOKEN ,token_by_recipient );
+// #[test]
+// fn try_withdraw_already_done_must_be_failed() {
+//     // Set up environment.
 
-    // withdraw_token    
-    let withdraw_receipt =  test_env.withdraw_token(not_recipient_key, not_recipient_address);
-    assert!(withdraw_receipt.success);
+//     let mut ledger = InMemorySubstateStore::with_bootstrap();
+//     let mut test_env = TestEnv::new(&mut ledger, RADIX_TOKEN);
+//     assert_eq!(
+//         test_env
+//             .get_balance(test_env.admin_account, RADIX_TOKEN)
+//             .unwrap(),
+//         dec!(1000000)
+//     );
+//     let (not_recipient_key, not_recipient_address) = test_env.new_account();
+//     let token_by_recipient: Decimal = dec!(100);
+//     // addRecipient
+//     test_env.add_recipient(not_recipient_address, RADIX_TOKEN, token_by_recipient);
 
-    let already_withdraw_receipt =  test_env.withdraw_token(not_recipient_key, not_recipient_address);
-    assert!(!already_withdraw_receipt.success);
-    
-    let log_message = &already_withdraw_receipt.logs.get(0).unwrap().1;
-    assert!(log_message.starts_with("Panicked at 'withdraw already done'"));
-}
+//     // withdraw_token
+//     let withdraw_receipt = test_env.withdraw_token(not_recipient_key, not_recipient_address);
+//     assert!(withdraw_receipt.success);
 
-#[test]
-fn try_withdraw_after_added_recipients_must_be_succeeded() {
-    // Set up environment.
+//     let already_withdraw_receipt =
+//         test_env.withdraw_token(not_recipient_key, not_recipient_address);
+//     assert!(!already_withdraw_receipt.success);
 
-    let mut ledger = InMemorySubstateStore::with_bootstrap();
-    let token_by_recipient : Decimal =  dec!(100); 
-    let mut test_env = TestEnv::new(&mut ledger, RADIX_TOKEN);
-    assert_eq!(test_env.get_balance(test_env.admin_account, RADIX_TOKEN).unwrap(),dec!(1000000));
+//     let log_message = &already_withdraw_receipt.logs.get(0).unwrap().1;
+//     assert!(log_message.starts_with("Panicked at 'withdraw already done'"));
+// }
 
-    // AddRecipients
-    let recipient_count  = 2; 
-    let mut recipients_accounts : Vec<(EcdsaPublicKey,Address)> =  Vec::new();
+// #[test]
+// fn try_withdraw_after_added_recipients_must_be_succeeded() {
+//     // Set up environment.
 
-    for _ in 0..recipient_count {
-        let (recipient_key, recipient_address)  = test_env.new_account();
-        let add_recipient_receipt = test_env.add_recipient(recipient_address, RADIX_TOKEN ,token_by_recipient );
-        assert!(add_recipient_receipt.result.is_ok()); 
-        recipients_accounts.push((recipient_key, recipient_address));  
-    }
+//     let mut ledger = InMemorySubstateStore::with_bootstrap();
+//     let token_by_recipient: Decimal = dec!(100);
+//     let mut test_env = TestEnv::new(&mut ledger, RADIX_TOKEN);
+//     assert_eq!(
+//         test_env
+//             .get_balance(test_env.admin_account, RADIX_TOKEN)
+//             .unwrap(),
+//         dec!(1000000)
+//     );
 
-    assert_eq!(test_env.get_balance(test_env.admin_account, RADIX_TOKEN).unwrap() , dec!(1000000) - token_by_recipient * recipient_count);
+//     // AddRecipients
+//     let recipient_count = 2;
+//     let mut recipients_accounts: Vec<(EcdsaPublicKey, Address)> = Vec::new();
 
-    
-    for (recipient_key, recipient_address)  in recipients_accounts {
-           // available token
-            let available_token_receipt = test_env.available_token(recipient_key, recipient_address); 
-            assert!(available_token_receipt.success); 
-            assert_eq!(format!("available : {}", token_by_recipient), available_token_receipt.logs.get(0).unwrap().1);
-            
-            // withdraw_token
-            let withdraw_receipt =  test_env.withdraw_token(recipient_key, recipient_address);
-            assert!(withdraw_receipt.success); 
-            assert_eq!(format!("withdraw_token : {}", token_by_recipient), withdraw_receipt.logs.get(0).unwrap().1);
-            assert_eq!(test_env.get_balance(recipient_address, RADIX_TOKEN).unwrap(), dec!(1000000) + token_by_recipient);
-    }
-}
+//     for _ in 0..recipient_count {
+//         let (recipient_key, recipient_address) = test_env.new_account();
+//         let add_recipient_receipt =
+//             test_env.add_recipient(recipient_address, RADIX_TOKEN, token_by_recipient);
+//         assert!(add_recipient_receipt.result.is_ok());
+//         recipients_accounts.push((recipient_key, recipient_address));
+//     }
 
-struct TestEnv<'a> {
-    executor: TransactionExecutor<'a, InMemorySubstateStore>,
-    admin_key: EcdsaPublicKey,
-    admin_account: Address,
-    component: Address,
-    admin_badge: Address,
-    recipient_badge: Address
-}
+//     assert_eq!(
+//         test_env
+//             .get_balance(test_env.admin_account, RADIX_TOKEN)
+//             .unwrap(),
+//         dec!(1000000) - token_by_recipient * recipient_count
+//     );
 
-impl<'a> TestEnv<'a> {
-    pub fn new(ledger: &'a mut InMemorySubstateStore, token_type : Address) -> Self {
-        let mut executor = TransactionExecutor::new(ledger, false);
+//     for (recipient_key, recipient_address) in recipients_accounts {
+//         // available token
+//         let available_token_receipt = test_env.available_token(recipient_key, recipient_address);
+//         assert!(available_token_receipt.success);
+//         assert_eq!(
+//             format!("available : {}", token_by_recipient),
+//             available_token_receipt.logs.get(0).unwrap().1
+//         );
 
-        let package = executor.publish_package(include_code!("airdrop_with_withdraw"));
-        let admin_key = executor.new_public_key();
-        let admin_account = executor.new_account(admin_key);
+//         // withdraw_token
+//         let withdraw_receipt = test_env.withdraw_token(recipient_key, recipient_address);
+//         assert!(withdraw_receipt.success);
+//         assert_eq!(
+//             format!("withdraw_token : {}", token_by_recipient),
+//             withdraw_receipt.logs.get(0).unwrap().1
+//         );
+//         assert_eq!(
+//             test_env
+//                 .get_balance(recipient_address, RADIX_TOKEN)
+//                 .unwrap(),
+//             dec!(1000000) + token_by_recipient
+//         );
+//     }
+// }
 
-        let tx = TransactionBuilder::new(&executor)
-            .call_function(package.unwrap(), "AirdropWithWithdraw", "new", vec![token_type.to_string()], Some(admin_account))
-            .call_method_with_all_resources(admin_account, "deposit_batch")
-            .build(vec![admin_key])
-            .unwrap();
+// struct TestEnv<'a> {
+//     executor: TransactionExecutor<'a, InMemorySubstateStore>,
+//     admin_key: EcdsaPublicKey,
+//     admin_account: Address,
+//     component: Address,
+//     admin_badge: Address,
+//     recipient_badge: Address,
+// }
 
-        let receipt = executor.run(tx).unwrap();
-        
-        println!("{:?}\n", receipt);
-        assert!(receipt.result.is_ok());
+// impl<'a> TestEnv<'a> {
+//     pub fn new(ledger: &'a mut InMemorySubstateStore, token_type: Address) -> Self {
+//         let mut executor = TransactionExecutor::new(ledger, false);
 
-        let admin_badge = receipt.resource_def(0).unwrap();
-        let recipient_badge = receipt.resource_def(1).unwrap();
+//         let package = executor.publish_package(include_code!("airdrop_with_withdraw"));
+//         let admin_key = executor.new_public_key();
+//         let admin_account = executor.new_account(admin_key);
 
-        Self {
-            executor,
-            admin_key,
-            admin_account,
-            component: receipt.component(0).unwrap(),
-            admin_badge,
-            recipient_badge
-        }
-    }
+//         let tx = TransactionBuilder::new(&executor)
+//             .call_function(
+//                 package.unwrap(),
+//                 "AirdropWithWithdraw",
+//                 "new",
+//                 vec![token_type.to_string()],
+//                 Some(admin_account),
+//             )
+//             .call_method_with_all_resources(admin_account, "deposit_batch")
+//             .build(vec![admin_key])
+//             .unwrap();
 
-    pub fn new_account(&mut self) -> (EcdsaPublicKey, Address) {
-        let key = self.executor.new_public_key();
-        (key, self.executor.new_account(key))
-    }
+//         let receipt = executor.run(tx).unwrap();
 
-    pub fn add_recipient(&mut self, recipient: Address, token_address: Address, tokens: Decimal) -> radix_engine::model::Receipt {
-        let tx = TransactionBuilder::new(&self.executor)
-            .call_method(
-                self.component,
-                "add_recipient",
-                vec![
-                    recipient.to_string(),
-                    format!("{},{}", tokens, token_address),
-                    format!("1,{}", self.admin_badge)
-                ],
-                Some(self.admin_account),
-            )
-            .call_method_with_all_resources(self.admin_account, "deposit_batch")
-            .build(vec![self.admin_key])
-            .unwrap();
-        let receipt = self.executor.run(tx).unwrap();
-        println!("{:?}\n", receipt);
-        assert!(receipt.result.is_ok());
+//         println!("{:?}\n", receipt);
+//         assert!(receipt.result.is_ok());
 
-        receipt
-    }
+//         let admin_badge = receipt.resource_def(0).unwrap();
+//         let recipient_badge = receipt.resource_def(1).unwrap();
 
-    fn perform_airdrop(&mut self, amount: i32, token: Address) -> radix_engine::model::Receipt {
-        let tx = TransactionBuilder::new(&self.executor)
-            .call_method(
-                self.component,
-                "perform_airdrop",
-                vec![
-                    format!("{},{}", amount, token),
-                    format!("1,{}", self.admin_badge)
-                ],
-                Some(self.admin_account),
-            )
-            .call_method_with_all_resources(self.admin_account, "deposit_batch")
-            .build(vec![self.admin_key])
-            .unwrap();
-        let receipt = self.executor.run(tx).unwrap();
-        println!("{:?}\n", receipt);
-        receipt
-    }
+//         Self {
+//             executor,
+//             admin_key,
+//             admin_account,
+//             component: receipt.component(0).unwrap(),
+//             admin_badge,
+//             recipient_badge,
+//         }
+//     }
 
-    fn get_balance(&self, account: Address, token: Address) -> Result<Decimal, String> {
-        let component = self.executor.ledger().get_component(account).unwrap();
-        let state = component.state();
-        let maps = radix_engine::engine::validate_data(state).unwrap().lazy_maps;
-        let resources = self.executor.ledger().get_lazy_map(&account, &maps[0]).unwrap();
-        let vault_id_data = resources.get_entry(&scrypto_encode(&token));
+//     pub fn new_account(&mut self) -> (EcdsaPublicKey, Address) {
+//         let key = self.executor.new_public_key();
+//         (key, self.executor.new_account(key))
+//     }
 
-        match vault_id_data {
-            Some(vault_id_data) => {
-                let vault_id = scrypto_decode::<Vid>(&vault_id_data).unwrap();
-                let vault = self.executor.ledger().get_vault(&account, &vault_id).unwrap();
-                Ok(vault.amount())
-            },
-            None => {
-                Err(format!(
-                    "No vault found for token {} in account {}",
-                    token, account
-                ))
-            }
-        }
-    }
-}
+//     pub fn add_recipient(
+//         &mut self,
+//         recipient: Address,
+//         token_address: Address,
+//         tokens: Decimal,
+//     ) -> radix_engine::model::Receipt {
+//         let tx = TransactionBuilder::new(&self.executor)
+//             .call_method(
+//                 self.component,
+//                 "add_recipient",
+//                 vec![
+//                     recipient.to_string(),
+//                     format!("{},{}", tokens, token_address),
+//                     format!("1,{}", self.admin_badge),
+//                 ],
+//                 Some(self.admin_account),
+//             )
+//             .call_method_with_all_resources(self.admin_account, "deposit_batch")
+//             .build(vec![self.admin_key])
+//             .unwrap();
+//         let receipt = self.executor.run(tx).unwrap();
+//         println!("{:?}\n", receipt);
+//         assert!(receipt.result.is_ok());
+
+//         receipt
+//     }
+
+//     fn perform_airdrop(&mut self, amount: i32, token: Address) -> radix_engine::model::Receipt {
+//         let tx = TransactionBuilder::new(&self.executor)
+//             .call_method(
+//                 self.component,
+//                 "perform_airdrop",
+//                 vec![
+//                     format!("{},{}", amount, token),
+//                     format!("1,{}", self.admin_badge),
+//                 ],
+//                 Some(self.admin_account),
+//             )
+//             .call_method_with_all_resources(self.admin_account, "deposit_batch")
+//             .build(vec![self.admin_key])
+//             .unwrap();
+//         let receipt = self.executor.run(tx).unwrap();
+//         println!("{:?}\n", receipt);
+//         receipt
+//     }
+
+//     fn get_balance(&self, account: Address, token: Address) -> Result<Decimal, String> {
+//         let component = self.executor.ledger().get_component(account).unwrap();
+//         let state = component.state();
+//         let maps = radix_engine::engine::validate_data(state)
+//             .unwrap()
+//             .lazy_maps;
+//         let resources = self
+//             .executor
+//             .ledger()
+//             .get_lazy_map(&account, &maps[0])
+//             .unwrap();
+//         let vault_id_data = resources.get_entry(&scrypto_encode(&token));
+
+//         match vault_id_data {
+//             Some(vault_id_data) => {
+//                 let vault_id = scrypto_decode::<Vid>(&vault_id_data).unwrap();
+//                 let vault = self
+//                     .executor
+//                     .ledger()
+//                     .get_vault(&account, &vault_id)
+//                     .unwrap();
+//                 Ok(vault.amount())
+//             }
+//             None => Err(format!(
+//                 "No vault found for token {} in account {}",
+//                 token, account
+//             )),
+//         }
+//     }
+// }
