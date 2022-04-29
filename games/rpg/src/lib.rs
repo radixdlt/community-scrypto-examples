72f41fe5c1404a2baa15d5d13ed9d533d995586a
Ethan Tran <ethantran.et77@gmail.com>
2022-04-29 09:09:59 -0700

    Update lib.rs

--- games/rpg/src/lib.rs
+++ games/rpg/src/lib.rs
@@ -14,12 +14,31 @@ pub enum Class {
 #[derive(NonFungibleData)]
 pub struct Account {
     #[scrypto(mutable)]
+    name: String,
     exp: Decimal,
     class: Class,
-    level: u8,
+    
 
 blueprint! {
     struct Substradix {
          gold_vault: Vault,
+    }
+
+    impl Substradix {
+        pub fn instantiate_nft() -> ComponentAddress {
+            // Creates a set of NFTs
+            let account_bucket = ResourceBuilder::new_non_fungible()
+                .metadata("name", "Substradix Account NFT")
+                .initial_supply([
+                    (
+                        NonFungibleId::from_u64(1u64),
+                        Account {
+                            name: Quwin,
+                            class: Class::Paladin,
+                            exp: 0,
+                        },
+                    ),
+                ]);
+
          
 
