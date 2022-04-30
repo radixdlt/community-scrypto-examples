9c13ec539106f1f5ba9649c65cb810d551a1e067
Ethan Tran <ethantran.et77@gmail.com>
2022-04-30 13:47:04 -0700

    Update lib.rs

--- games/rpg/src/lib.rs
+++ games/rpg/src/lib.rs
@@ -28,11 +28,11 @@ blueprint! {
          // Vault to hold with badge for system actions
          system_vault: Vault,
          // Version ID to allow updates post instantiation, older NFTs can't be used in updated versions without updating the NFT. Prevents exploits.
-         version: Decimal,
+         version:  u8,
     }
 
     impl Substradix {
-        pub fn new(game_price: Decimal, version: Decimal) -> ComponentAddress {
+        pub fn new(game_price: Decimal) -> ComponentAddress {
             // Creates system badge for minting + verifying level outcomes.
             let system_badge = ResourceBuilder::new_fungible()
                 .metadata("name", "system")
@@ -50,11 +50,11 @@ blueprint! {
                 .no_initial_supply();
 
             // Gold for ingame currency
-            let token_gold: Bucket = ResourceBuilder::new_fungible()
+            let token_gold = ResourceBuilder::new_fungible()
                 .metadata("name", "Gold Coin")
                 .mintable(rule!(require(system_badge.resource_address())), LOCKED)
                 .burnable(rule!(require(system_badge.resource_address())), LOCKED)
-                .initial_supply(1);
+                .no_initial_supply();
                     
 
             // Creates materials for upgrading  
@@ -74,7 +74,7 @@ blueprint! {
             // Sets values for instantiation
             Self {
                 system_vault: Vault::with_bucket(system_badge),
-                gold_vault: Vault::with_bucket(token_gold),
+                gold_vault: Vault::new(token_gold),
                 collected_xrd: Vault::new(RADIX_TOKEN),
                 game_price,
                 character_number: 0,
@@ -86,36 +86,39 @@ blueprint! {
             .globalize()
         }
 
+        pub fn check_nft_resource_address(&self) {
+            self.character_nft;
+        }
+
         // Creates character
-        pub fn create_character(&mut self,
-            class: u8,
-            health: u32,
-            attack: u32,
-            magic: u32,
-            defense: u32,
-            speed: u32,
-            new_character: Bucket,
+        pub fn create_character_gladiator(&mut self,
+            mut new_character: Bucket,
             mut payment: Bucket,
             ) -> (Bucket, Bucket) {
 
             // Prevents making alt accounts on one wallet.
-            assert!(new_character.amount() == dec!(0),
-                "You cannot have more than one character per account!");
-
+            assert!(
+                new_character.amount() == dec!(0),
+                "You cannot have more than one character per account!"
+            );
+            assert!(
+                new_character.resource_address() == self.character_nft,
+                "Wrong Resource Address!"
+            );
             let character_data = Account {
-                class: class,
+                class: 1,
                 exp: 0,
-                health: health,
-                attack: attack,
-                magic: magic,
-                defense: defense,
-                speed: speed,
+                health: 11,
+                attack: 11,
+                magic: 8,
+                defense: 10,
+                speed: 10,
             };
 
-            let new_character = self.authorize(|| {
+            let new_character = {
                 borrow_resource_manager!(self.character_nft)
                     .mint_non_fungible(&NonFungibleId::from_u64(self.character_number), character_data)
-            });
+            };
             self.character_number += 1;
             self.collected_xrd.put(payment.take(self.game_price));
             (new_character, payment)
@@ -148,9 +151,9 @@ blueprint! {
 
         }
 
-        pub fn level_1(&mut self) -> Bucket {
+        pub fn level_1(&mut self, nft_character: Bucket) -> Bucket {
             let seed = Runtime::generate_uuid();
-            let damage_rng = 
+            let damage_rng = e;
             let mut nft_data: Account = nft_character.non_fungible().data();
             let health: u32 = nft_data.health;
             let attack: u32 = nft_data.attack;
@@ -160,8 +163,8 @@ blueprint! {
             let enemy_data = bruh;
     
             loop {
-                let health = health - ((enemy_attack - defense) *  );
-                let enemy_health = enemy_health - ((attack - enemy_defense) *  );
+                let health = health - ((enemy_attack - defense) * 1);
+                let enemy_health = enemy_health - ((attack - enemy_defense) * 1);
 
                 if health <=1 {
                     println!("You Died");
@@ -176,6 +179,6 @@ blueprint! {
 
                 }
             }
-        }
+        }       
     }
 }
