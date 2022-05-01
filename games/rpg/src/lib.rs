a69e0c914c81c75b828319156bc4536a99fb3bd5
Ethan Tran <ethantran.et77@gmail.com>
2022-04-30 19:53:23 -0700

    Update lib.rs

--- games/rpg/src/lib.rs
+++ games/rpg/src/lib.rs
@@ -4,11 +4,17 @@ use scrypto::prelude::*;
 pub struct Account {
     #[scrypto(mutable)]
     class: u8,
+    #[scrypto(mutable)]
     exp: u128,
+    #[scrypto(mutable)]
     health: u32,
+    #[scrypto(mutable)]
     attack: u32,
+    #[scrypto(mutable)]
     magic: u32,
+    #[scrypto(mutable)]
     defense: u32,
+    #[scrypto(mutable)]
     speed: u32,
 }
 
@@ -29,6 +35,7 @@ blueprint! {
          system_vault: Vault,
          // Version ID to allow updates post instantiation, older NFTs can't be used in updated versions without updating the NFT. Prevents exploits.
          version:  u8,
+
     }
 
     impl Substradix {
@@ -37,15 +44,17 @@ blueprint! {
             let system_badge = ResourceBuilder::new_fungible()
                 .metadata("name", "system")
                 .divisibility(DIVISIBILITY_NONE)
-                .initial_supply(1);
+                .initial_supply(200000000);
 
             // NFT for character data
             let character_nft = ResourceBuilder::new_non_fungible()
                 .metadata("type", "Substradix character NFT")
                 .mintable(AccessRule::AllowAll, LOCKED)
                 .burnable(AccessRule::AllowAll, LOCKED)
+                .restrict_withdraw(rule!(require(system_badge.resource_address())), LOCKED)
+                .restrict_deposit(rule!(require(system_badge.resource_address())), LOCKED)
                 .updateable_non_fungible_data(AccessRule::AllowAll, LOCKED)
-                .restrict_deposit(AccessRule::DenyAll, LOCKED)
+
                 .restrict_withdraw(AccessRule::DenyAll, LOCKED)
                 .no_initial_supply();
 
@@ -87,25 +96,17 @@ blueprint! {
         }
 
         pub fn check_nft_resource_address(&self) {
-            self.character_nft;
+            println!("To mint an NFT, use resource address {}", self.character_nft)
         }
 
         // Creates character
         pub fn create_character_gladiator(&mut self,
-            mut new_character: Bucket,
             mut payment: Bucket,
             ) -> (Bucket, Bucket) {
+            
+            let mut key_bucket: Bucket = self.system_vault.take(1);
 
-            // Prevents making alt accounts on one wallet.
-            assert!(
-                new_character.amount() == dec!(0),
-                "You cannot have more than one character per account!"
-            );
-            assert!(
-                new_character.resource_address() == self.character_nft,
-                "Wrong Resource Address!"
-            );
-            let character_data = Account {
+            let character_data = Account {  
                 class: 1,
                 exp: 0,
                 health: 11,
@@ -115,12 +116,14 @@ blueprint! {
                 speed: 10,
             };
 
-            let new_character = {
+            let new_character = self.system_vault.authorize(|| 
                 borrow_resource_manager!(self.character_nft)
                     .mint_non_fungible(&NonFungibleId::from_u64(self.character_number), character_data)
-            };
+            );
+
             self.character_number += 1;
             self.collected_xrd.put(payment.take(self.game_price));
+            self.system_vault.put(key_bucket.take(1));
             (new_character, payment)
         }
 
@@ -136,6 +139,11 @@ blueprint! {
         }
 
         pub fn game_level(&mut self,  nft_character: Bucket, exp_gain: u128) -> Bucket {
+            assert!(
+                nft_character.resource_address() == self.character_nft,
+                "Wrong resource address!"
+            );
+
             let seed = Runtime::generate_uuid();
 
             let _nft_key = nft_character.non_fungible::<Account>().id();
@@ -153,32 +161,85 @@ blueprint! {
 
         pub fn level_1(&mut self, nft_character: Bucket) -> Bucket {
             let seed = Runtime::generate_uuid();
-            let damage_rng = e;
             let mut nft_data: Account = nft_character.non_fungible().data();
             let health: u32 = nft_data.health;
             let attack: u32 = nft_data.attack;
             let magic: u32 = nft_data.magic;
             let defense: u32 = nft_data.defense;
             let speed: u32 = nft_data.speed;
-            let enemy_data = bruh;
-    
-            loop {
-                let health = health - ((enemy_attack - defense) * 1);
-                let enemy_health = enemy_health - ((attack - enemy_defense) * 1);
+            let enemy_health: u32 = 10;
+            let enemy_attack: u32 = 10;
+            let enemy_magic: u32 = 10;
+            let enemy_defense: u32 = 10;
+            let enemy_speed: u32 = 10;
 
-                if health <=1 {
+            loop {
+                if speed <= enemy_speed {
+                    let enemy_health = enemy_health - ((attack - enemy_defense) * 1);
+                    let health = health - ((enemy_attack - defense) * 1);
+                }
+                else {
+                    let health = health - ((enemy_attack - defense) * 1);
+                    let enemy_health = enemy_health - ((attack - enemy_defense) * 1);
+                }
+
+                if health <= 1 {
                     println!("You Died");
                     break;
+
                 }
-  
+        
                 if enemy_health <=1 {
                     println!("Victory!");
                     nft_data.exp += 15;
-                    continue;
-                    
-
+                    break;
                 }
             }
-        }       
+
+                loop {
+                    if speed <= enemy_speed {
+                        let enemy_health = enemy_health - ((attack - enemy_defense) * 1);
+                        let health = health - ((enemy_attack - defense) * 1);
+                    }
+                    else {
+                        let health = health - ((enemy_attack - defense) * 1);
+                        let enemy_health = enemy_health - ((attack - enemy_defense) * 1);
+                    }
+
+                    if health <= 1 {
+                        println!("You Died");
+                        break;
+                    }
+
+                    if enemy_health <=1 {
+                        println!("Victory!");
+                        nft_data.exp += 15;
+                        break;
+                    }
+                }
+
+                    loop {
+                        if speed <= enemy_speed {
+                            let enemy_health = enemy_health - ((attack - enemy_defense) * 1);
+                            let health = health - ((enemy_attack - defense) * 1);
+                        }
+                        else {
+                            let health = health - ((enemy_attack - defense) * 1);
+                            let enemy_health = enemy_health - ((attack - enemy_defense) * 1);
+                        }
+        
+                        if health <= 1 {
+                            println!("You Died");
+                            break;
+                        }
+                
+                        if enemy_health <=1 {
+                            println!("Victory!");
+                            nft_data.exp += 15;
+                            break;
+                        }
+                    }
+        nft_character            
+        }
     }
 }
