bc17f424dc6562fdf4970b3f80e48bc7950850ea
Ethan Tran <ethantran.et77@gmail.com>
2022-05-01 20:29:19 -0700

    Update lib.rs

--- games/rpg/src/lib.rs
+++ games/rpg/src/lib.rs
@@ -1,21 +1,30 @@
 use scrypto::prelude::*;
 
+pub mod combat;
+
 #[derive(NonFungibleData)]
 pub struct Account {
+    // Classes are: 
+    // #1 = Gladiator: High attack damage, medium defense, medium speed. Can be built towards Lifesteal identity (Heal from damage, stronger activates <50% HP), or Perfect Warrior identity (attacks can combo, stronger >50% HP)
+    // #2 = Rogue: Hybrid attack/magic damage, High Speed. All rounder, able to be built towards both max attack (crit burst identity) or max magic (shadow magic dps identity)
+    // #3 = Ranger: Ranged Attack Damage, medium attack medium speed. Single-target ranged dps identity. 
+    // #4 = Mystic: Ranged Magic Damage, High magic low defense, low speed. AOE ranged burst identity.
+    // #5 = Vanguard: High Health, High Defense. Can be built towards risky Counter Identity(attack burst damage) or Suffocation Identity(magic dps damage + max tanky)
+    // More Classes can be added later
     #[scrypto(mutable)]
-    class: u8,
+    class: u8, 
     #[scrypto(mutable)]
     exp: u128,
     #[scrypto(mutable)]
-    health: u32,
+    health: i32,
     #[scrypto(mutable)]
-    attack: u32,
+    attack: i32,
     #[scrypto(mutable)]
-    magic: u32,
+    magic: i32,
     #[scrypto(mutable)]
-    defense: u32,
+    defense: i32,
     #[scrypto(mutable)]
-    speed: u32,
+    speed: i32,
 }
 
 
@@ -35,54 +44,65 @@ blueprint! {
          system_vault: Vault,
          // Version ID to allow updates post instantiation, older NFTs can't be used in updated versions without updating the NFT. Prevents exploits.
          version:  u8,
+         developer_vault: Vault,
 
     }
 
     impl Substradix {
-        pub fn new(game_price: Decimal) -> ComponentAddress {
-            // Creates system badge for minting + verifying level outcomes.
+        pub fn new(game_price: Decimal) -> (ComponentAddress, Bucket) {
+
+            // Creates developer badge for methods. Necessary to control system_badge
+            let mut developer_badge = ResourceBuilder::new_fungible()
+                .metadata("name", "developer")
+                .divisibility(DIVISIBILITY_NONE)
+                .initial_supply(10000);
+
+            let developer_rule: AccessRule = rule!(require(developer_badge.resource_address()));
+
+            // Creates system badge changing NFT Data. Necessary for game expansions.
             let system_badge = ResourceBuilder::new_fungible()
                 .metadata("name", "system")
                 .divisibility(DIVISIBILITY_NONE)
-                .initial_supply(200000000);
-            
+                .mintable(developer_rule.clone(), MUTABLE(developer_rule.clone()))
+                .initial_supply(1000000);
+
             let system_rule: AccessRule = rule!(require(system_badge.resource_address()));
 
             // NFT for character data
             let character_nft = ResourceBuilder::new_non_fungible()
                 .metadata("type", "Substradix character NFT")
-                .mintable(system_rule.clone(), LOCKED)
-                .burnable(system_rule.clone(), LOCKED)
-                //.restrict_deposit(system_rule.clone(), MUTABLE(system_rule.clone()))
-                .restrict_withdraw(system_rule.clone(), MUTABLE(system_rule.clone()))
-                .updateable_non_fungible_data(system_rule.clone(), MUTABLE(system_rule.clone()))
+                .mintable(system_rule.clone(), MUTABLE(developer_rule.clone()))
+                .burnable(system_rule.clone(), MUTABLE(developer_rule.clone()))
+                .restrict_withdraw(AccessRule::DenyAll, MUTABLE(developer_rule.clone()))
+                .updateable_non_fungible_data(system_rule.clone(), MUTABLE(developer_rule.clone()))
                 .no_initial_supply();
 
             // Gold for ingame currency
             let token_gold = ResourceBuilder::new_fungible()
                 .metadata("name", "Gold Coin")
-                .mintable(rule!(require(system_badge.resource_address())), LOCKED)
-                .burnable(rule!(require(system_badge.resource_address())), LOCKED)
+                .mintable(system_rule.clone(), MUTABLE(developer_rule.clone()))
+                .burnable(system_rule.clone(), MUTABLE(developer_rule.clone()))
                 .no_initial_supply();
                     
 
             // Creates materials for upgrading  
-            let _token_greavite = ResourceBuilder::new_fungible()
+            let token_greavite = ResourceBuilder::new_fungible()
                 .metadata("name", "Greavite Ore")
-                .mintable(rule!(require(system_badge.resource_address())), LOCKED)
-                .burnable(rule!(require(system_badge.resource_address())), LOCKED)
+                .mintable(rule!(require(system_badge.resource_address())), MUTABLE(developer_rule.clone()))
+                .burnable(rule!(require(system_badge.resource_address())), MUTABLE(developer_rule.clone()))
                 .no_initial_supply();
 
 
-            let _token_wood = ResourceBuilder::new_fungible()
+            let token_wood = ResourceBuilder::new_fungible()
                 .metadata("name", "Ageis Wood")
-                .mintable(rule!(require(system_badge.resource_address())), LOCKED)
-                .burnable(rule!(require(system_badge.resource_address())), LOCKED)
+                .mintable(rule!(require(system_badge.resource_address())), MUTABLE(developer_rule.clone()))
+                .burnable(rule!(require(system_badge.resource_address())), MUTABLE(developer_rule.clone()))
                 .no_initial_supply();
 
             // Sets values for instantiation
-            Self {
+            let instantiate = Self {
                 system_vault: Vault::with_bucket(system_badge),
+                developer_vault: Vault::with_bucket(developer_badge.take(9990)),
                 gold_vault: Vault::new(token_gold),
                 collected_xrd: Vault::new(RADIX_TOKEN),
                 game_price,
@@ -92,18 +112,14 @@ blueprint! {
 
             }
             .instantiate()
-            .globalize()
+            .globalize();
+            (instantiate, developer_badge)
         }
 
-        pub fn check_nft_resource_address(&self) {
-            println!("To mint an NFT, use resource address {}", self.character_nft)
-        }
-
-        // Creates character
-        pub fn create_character_gladiator(&mut self,
+        // Creates Gladiator
+        pub fn create_character_class1(&mut self,
             mut payment: Bucket,
             ) -> (Bucket, Bucket, ) {
-            
             let key_bucket: Bucket = self.system_vault.take(1);
 
             let character_data = Account {  
@@ -120,95 +136,212 @@ blueprint! {
                 borrow_resource_manager!(self.character_nft)
                     .mint_non_fungible(&NonFungibleId::from_u64(self.character_number), character_data));
             
+            self.character_number += 1;
+            self.collected_xrd.put(payment.take(self.game_price));
+            self.system_vault.put(key_bucket);
+            (new_character, payment,)
+
+        }
+        // Creates Rogue
+        pub fn create_character_class2(&mut self,
+            mut payment: Bucket,
+            ) -> (Bucket, Bucket, ) {
+            let key_bucket: Bucket = self.system_vault.take(1);
+
+            let character_data = Account {  
+                class: 2,
+                exp: 0,
+                health: 9,
+                attack: 11,
+                magic: 11,
+                defense: 8,
+                speed: 11,
+            };
+
+            let new_character = self.system_vault.authorize(||
+                borrow_resource_manager!(self.character_nft)
+                    .mint_non_fungible(&NonFungibleId::from_u64(self.character_number), character_data));
             
             self.character_number += 1;
             self.collected_xrd.put(payment.take(self.game_price));
-
-            let whyyy:Bucket = new_character;
             self.system_vault.put(key_bucket);
-            (whyyy, payment,)
+            (new_character, payment,)
 
         }
 
         // Owner only, collects all XRD from sold Personal Tokens
         pub fn withdraw_xrd(&mut self) -> Bucket {
-            self.collected_xrd.take_all()
+            let withdraw = self.developer_vault.authorize(||self.collected_xrd.take_all());
+            withdraw
         }
 
         // Changes price of Substradix
-         pub fn change_price(&mut self, new_price: Decimal) -> Decimal {
-            self.game_price = new_price;
-            self.game_price
+         pub fn change_price(&mut self, new_price: Decimal) {
+            self.developer_vault.authorize(||self.game_price = new_price);
         }
 
-        pub fn game_level(&mut self,  nft_character: Bucket, exp_gain: u128) -> Bucket {
+        pub fn level_1(&mut self, nft_proof: Proof) -> Proof {
             assert!(
-                nft_character.resource_address() == self.character_nft,
+                nft_proof.amount() == dec!("1"),
+                "You can only fight with one character at once!"
+            );
+
+            assert!(
+                nft_proof.resource_address() == self.character_nft,
                 "Wrong resource address!"
             );
 
-            let seed = Runtime::generate_uuid();
+            //let key_bucket: Bucket = self.system_vault.take(1);
 
-            let _nft_key = nft_character.non_fungible::<Account>().id();
-
-            let mut nft_data: Account = nft_character.non_fungible().data();
-            nft_data.exp += exp_gain;
-
-            nft_character.non_fungible().update_data(nft_data);
-
-            println!("Your seed is {}", seed);
-
-            nft_character
-
-        }
-
-        pub fn level_1(&mut self, nft_character: Bucket) -> Bucket {
-            let seed = Runtime::generate_uuid();
-            let mut nft_data: Account = nft_character.non_fungible().data();
-            let health: u32 = nft_data.health;
-            let attack: u32 = nft_data.attack;
-            let magic: u32 = nft_data.magic;
-            let defense: u32 = nft_data.defense;
-            let speed: u32 = nft_data.speed;
-            let enemy_health: u32 = 10;
-            let enemy_attack: u32 = 10;
-            let enemy_magic: u32 = 10;
-            let enemy_defense: u32 = 10;
-            let enemy_speed: u32 = 10;
+            let _seed = Runtime::generate_uuid();
+            let mut nft_data: Account = nft_proof.non_fungible().data();
+            let mut health: i32 = nft_data.health;
+            let mut attack: i32 = nft_data.attack;
+            //let mut magic: i32 = nft_data.magic;
+            let mut defense: i32 = nft_data.defense;
+            let mut speed: i32 = nft_data.speed;
+            let mut enemy_health: i32 = 10;
+            let mut enemy_attack: i32 = 11;
+            //let mut enemy_magic: i32 = 0;
+            let mut enemy_defense: i32 = 3;
+            let mut enemy_speed: i32 = 8;
             // Fight simulator
-            if speed <= enemy_speed {
+            // Enemy 1:
+            if enemy_speed < speed {
+                loop {
+                    enemy_health -= attack - enemy_defense;
+                    if enemy_health <=0 {
+                        self.system_vault.authorize(||nft_data.exp += 15);
+                        break
+                    }
+                    health -= enemy_attack - defense;
+                    if health <= 0 {
+                        self.system_vault.authorize(||nft_data.exp += 1);
+                        return nft_proof
+                    }
+                };
+            }
+            else if speed < enemy_speed {
                 loop{
-                    let enemy_health = enemy_health - (attack - enemy_defense);
-                    let health = health - (enemy_attack - defense);
+                    health -= enemy_attack - defense;
                     if health <= 1 {
-                        println!("You Died");
-                        return nft_character
+                        self.system_vault.authorize(||nft_data.exp += 1);
+                        return nft_proof
                     }
+                    enemy_health -= attack - enemy_defense;
                     if enemy_health <=1 {
-                        println!("Victory!");
-                        nft_data.exp += 15;
-                        break;
+                        self.system_vault.authorize(||nft_data.exp += 15);
+                        break
+        
                     }
-                }
-
+                };
             }
             else {
                 loop{
-                    let health = health - (enemy_attack - defense);
-                    let enemy_health = enemy_health - (attack - enemy_defense);
-                    if health <= 1 {
-                        println!("You Died");
-                        return nft_character
-                    }
+                    enemy_health -= attack - enemy_defense;
                     if enemy_health <=1 {
-                        println!("Victory!");
-                        nft_data.exp += 15;
-                        break;
+                        self.system_vault.authorize(||nft_data.exp += 15);
+                        break
+        
                     }
-                }
+                    health -= enemy_attack - defense;
+                    if health <= 1 {
+                        self.system_vault.authorize(||nft_data.exp += 1);
+                        return nft_proof
+                    }
+                };
             }
-
-            nft_character            
+            // Enemy 2:
+            if enemy_speed < speed {
+                loop {
+                    enemy_health -= attack - enemy_defense;
+                    if enemy_health <=0 {
+                        self.system_vault.authorize(||nft_data.exp += 15);
+                        break
+                    }
+                    health -= enemy_attack - defense;
+                    if health <= 0 {
+                        self.system_vault.authorize(||nft_data.exp += 1);
+                        return nft_proof
+                    }
+                };
+            }
+            else if speed < enemy_speed {
+                loop{
+                    health -= enemy_attack - defense;
+                    if health <= 1 {
+                        self.system_vault.authorize(||nft_data.exp += 1);
+                        return nft_proof
+                    }
+                    enemy_health -= attack - enemy_defense;
+                    if enemy_health <=1 {
+                        self.system_vault.authorize(||nft_data.exp += 15);
+                        break
+        
+                    }
+                };
+            }
+            else {
+                loop{
+                    enemy_health -= attack - enemy_defense;
+                    if enemy_health <=1 {
+                        self.system_vault.authorize(||nft_data.exp += 15);
+                        break
+        
+                    }
+                    health -= enemy_attack - defense;
+                    if health <= 1 {
+                        self.system_vault.authorize(||nft_data.exp += 1);
+                        return nft_proof
+                    }
+                };
+            }
+            // Enemy 3:
+            if enemy_speed < speed {
+                loop {
+                    enemy_health -= attack - enemy_defense;
+                    if enemy_health <=0 {
+                        self.system_vault.authorize(||nft_data.exp += 15);
+                        break
+                    }
+                    health -= enemy_attack - defense;
+                    if health <= 0 {
+                        self.system_vault.authorize(||nft_data.exp += 1);
+                        return nft_proof
+                    }
+                };
+            }
+            else if speed < enemy_speed {
+                loop{
+                    health -= enemy_attack - defense;
+                    if health <= 1 {
+                        self.system_vault.authorize(||nft_data.exp += 1);
+                        return nft_proof
+                    }
+                    enemy_health -= attack - enemy_defense;
+                    if enemy_health <=1 {
+                        self.system_vault.authorize(||nft_data.exp += 15);
+                        break
+        
+                    }
+                };
+            }
+            else {
+                loop{
+                    enemy_health -= attack - enemy_defense;
+                    if enemy_health <=1 {
+                        self.system_vault.authorize(||nft_data.exp += 15);
+                        break
+        
+                    }
+                    health -= enemy_attack - defense;
+                    if health <= 1 {
+                        self.system_vault.authorize(||nft_data.exp += 1);
+                        return nft_proof
+                    }
+                };
+            }
+            return nft_proof
         }
     }
 }
