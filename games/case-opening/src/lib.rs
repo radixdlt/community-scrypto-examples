49b166a706438d5b1c342d123b44bf272e3ed829
Ethan Tran <ethantran.et77@gmail.com>
2022-05-10 08:52:25 -0700

    Update lib.rs

--- games/case-opening/src/lib.rs
+++ games/case-opening/src/lib.rs
@@ -1,2 +1,13 @@
 use scrypto::prelude::*;
 
+#[derive(NonFungibleData)]
+pub struct Skin {
+    name: String,
+    weapon: String,
+    float: u128,
+    pattern: u16,
+    
+}
+
+blueprint! {
+    struct Substradix {
