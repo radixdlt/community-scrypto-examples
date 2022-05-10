e2b2aeafb8e9414b8811b388ac18d9ca20ce846c
Ethan Tran <ethantran.et77@gmail.com>
2022-05-10 08:59:33 -0700

    Update lib.rs

--- games/case-opening/src/lib.rs
+++ games/case-opening/src/lib.rs
@@ -10,4 +10,27 @@ pub struct Skin {
 }
 
 blueprint! {
-    struct Substradix {
+    struct Case {
+        system_badge: ResourceAddress,
+        system_vault: Vault,
+        skin_nft: ResourceAddress,
+        xrd_vault: Vault,
+    }
+    impl Case {
+        pub fn new() -> ComponentAddress {
+             
+
+
+
+
+
+            let skin_nft = ResourceBuilder::new_non_fungible()
+                .metadata("type", "Substradix character NFT")
+                .mintable(system_rule.clone(), MUTABLE(developer_rule.clone()))
+                .burnable(system_rule.clone(), MUTABLE(developer_rule.clone()))
+                .restrict_withdraw(AccessRule::DenyAll, MUTABLE(developer_rule.clone()))
+                .updateable_non_fungible_data(system_rule.clone(), MUTABLE(developer_rule.clone()))
+                .no_initial_supply(); 
+
+    }
+        
