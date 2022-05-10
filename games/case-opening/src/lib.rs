e4b721d9edc31250591d7088fa96e435e3b5ad32
Ethan Tran <ethantran.et77@gmail.com>
2022-05-10 09:01:06 -0700

    Update lib.rs

--- games/case-opening/src/lib.rs
+++ games/case-opening/src/lib.rs
@@ -18,11 +18,23 @@ blueprint! {
     }
     impl Case {
         pub fn new() -> ComponentAddress {
-             
+
+ // Creates developer badge for methods. Necessary to control system_badge
+            let mut developer_badge = ResourceBuilder::new_fungible()
+                .metadata("name", "developer")
+                .divisibility(DIVISIBILITY_NONE)
+                .initial_supply(10000);
+
+            let developer_rule: AccessRule = rule!(require(developer_badge.resource_address()));
 
 
+            let system_badge = ResourceBuilder::new_fungible()
+                .metadata("name", "system")
+                .divisibility(DIVISIBILITY_NONE)
+                .mintable(developer_rule.clone(), MUTABLE(developer_rule.clone()))
+                .initial_supply(1000000);
 
-
+            let system_rule: AccessRule = rule!(require(system_badge.resource_address()));
 
             let skin_nft = ResourceBuilder::new_non_fungible()
                 .metadata("type", "Substradix character NFT")
